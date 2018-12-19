8128f0a917b786fcf3c3ce1c814d46a09a935ec2
Sri Harsha <harrsha7@gmail.com>
2018-12-19 09:26:51 -0600

    Theme Applied

--- src/App.js
+++ src/App.js
@@ -7,20 +7,41 @@ import ButtonAppBar from "./views/Home"
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import TransitionTime from "./views/TransitionViews/TransitionTime";
 
+import  blue from "@material-ui/core/colors/blue"
+import amber from "@material-ui/core/colors/amber";
+import {createMuiTheme, MuiThemeProvider, withStyles} from "@material-ui/core/styles";
+
+const styles = createMuiTheme({
+    palette: {
+        primary: {
+            light: blue[300],
+            main: blue[500],
+            dark: blue[700],
+            textColor: '#fff',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: amber[300],
+            main: amber[500],
+            dark: amber[700],
+            contrastText: '#000',
+        },
+    }
+});
 class App extends Component {
   render() {
     return (
         <BrowserRouter>
-            <div>
-                <Route exact path="/" component={Home}/>
-                <Route
-                    path="/Magic8Menu"
-                    render={() => (<Magic8Menu/>)}
-                />
-            </div>
+                <MuiThemeProvider theme={styles}>
+                    <Route exact path="/" component={Home}/>
+                    <Route
+                        path="/Magic8Menu"
+                        render={() => (<Magic8Menu/>)}
+                    />
+                </MuiThemeProvider>
         </BrowserRouter>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
