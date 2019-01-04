55fe54aca8b1e58261034d09c6c396900b8432d4
Thomas Marosz <thomas.c.marosz@vanderbilt.edu>
2019-01-04 15:43:54 -0600

    prettify app js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import WelcomePage from "./views/WelcomeViews/Welcome";
 import ClassroomClimate from "./views/protected/ClassroomClimateViews/ClassroomClimate";
-import Magic8Menu from "./views/protected/Magic8Menu"
+import Magic8Menu from "./views/protected/Magic8Menu";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import TransitionTime from "./views/protected/TransitionViews/TransitionTime";
 import ForgotPasswordPage from "./views/ForgotPasswordViews/ForgotPassword";
 import HomePage from "./views/protected/HomeViews/Home";
 
-import  blue from "@material-ui/core/colors/blue"
+import blue from "@material-ui/core/colors/blue";
 import amber from "@material-ui/core/colors/amber";
-import {createMuiTheme, MuiThemeProvider, withStyles} from "@material-ui/core/styles";
+import {
+    createMuiTheme,
+    MuiThemeProvider,
+    withStyles
+} from "@material-ui/core/styles";
 
 const styles = createMuiTheme({
     palette: {
@@ -18,92 +22,129 @@ const styles = createMuiTheme({
             light: blue[300],
             main: blue[500],
             dark: blue[700],
-            textColor: '#fff',
-            contrastText: '#fff',
+            textColor: "#fff",
+            contrastText: "#fff"
         },
         secondary: {
             light: amber[300],
             main: amber[500],
             dark: amber[700],
-            contrastText: '#000',
-        },
+            contrastText: "#000"
+        }
     }
 });
 
-function PrivateRoute ({component: Component, auth, ...rest}) {
+function PrivateRoute({ component: Component, auth, ...rest }) {
     return (
         <Route
             {...rest}
-            render={(props) => auth === true
-                ? <Component {...props} />
-                : <Redirect to={{pathname: '/', state: {from: props.location}}} />}
+            render={props =>
+                auth === true ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect
+                        to={{ pathname: "/", state: { from: props.location } }}
+                    />
+                )
+            }
         />
-    )
+    );
 }
 
-function PublicRoute ({component: Component, auth, ...rest}) {
+function PublicRoute({ component: Component, auth, ...rest }) {
     return (
         <Route
             {...rest}
-            render={(props) => auth === false
-                ? <Component {...props} />
-                : <Redirect to='/Home' />}
+            render={props =>
+                auth === false ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to="/Home" />
+                )
+            }
         />
-    )
+    );
 }
-
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             auth: false,
             loading: true
-        }
+        };
     }
 
-    componentDidMount () {
-        this.removeListener = this.props.firebase.auth.onAuthStateChanged((user) => {
-            if (user) {
-                this.setState({
-                    auth: true,
-                    loading: false,
-                })
-            } else {
-                this.setState({
-                    auth: false,
-                    loading: false
-                })
+    componentDidMount() {
+        this.removeListener = this.props.firebase.auth.onAuthStateChanged(
+            user => {
+                if (user) {
+                    this.setState({
+                        auth: true,
+                        loading: false
+                    });
+                } else {
+                    this.setState({
+                        auth: false,
+                        loading: false
+                    });
+                }
             }
-        })
+        );
     }
 
-    componentWillUnmount () {
-        this.removeListener()
+    componentWillUnmount() {
+        this.removeListener();
     }
 
     render() {
-    return this.state.loading === true ? <h1>Loading</h1> : (
-                <BrowserRouter>
-                    <MuiThemeProvider theme={styles}>
-                        <Switch>
-                            <Route exact path='/' component={WelcomePage}/>
-                            <Route exact path='/forgot' component={ForgotPasswordPage}/>
-                            <PrivateRoute auth={this.state.auth} path='/Invite' component={HomePage}/>
-                            <PrivateRoute auth={this.state.auth} path='/Account' component={HomePage}/>
-                            <PrivateRoute auth={this.state.auth} path='/Home' component={HomePage}/>
-                            <PrivateRoute auth={this.state.auth} path='/TransitionTime' component={TransitionTime}/>
-    <PrivateRoute
-    auth={this.state.auth}
-    path="/ClassroomClimate"
-    component={ClassroomClimate}
-    />
-                            <PrivateRoute auth={this.state.auth} path='/Magic8Menu' component={Magic8Menu}/>
-                            <Route render={() => <h3>No Match</h3>}/>
-                        </Switch>
-                    </MuiThemeProvider>
-                </BrowserRouter>
-            )
+        return this.state.loading === true ? (
+            <h1>Loading</h1>
+        ) : (
+            <BrowserRouter>
+                <MuiThemeProvider theme={styles}>
+                    <Switch>
+                        <Route exact path="/" component={WelcomePage} />
+                        <Route
+                            exact
+                            path="/forgot"
+                            component={ForgotPasswordPage}
+                        />
+                        <PrivateRoute
+                            auth={this.state.auth}
+                            path="/Invite"
+                            component={HomePage}
+                        />
+                        <PrivateRoute
+                            auth={this.state.auth}
+                            path="/Account"
+                            component={HomePage}
+                        />
+                        <PrivateRoute
+                            auth={this.state.auth}
+                            path="/Home"
+                            component={HomePage}
+                        />
+                        <PrivateRoute
+                            auth={this.state.auth}
+                            path="/TransitionTime"
+                            component={TransitionTime}
+                        />
+                        <PrivateRoute
+                            auth={this.state.auth}
+                            path="/ClassroomClimate"
+                            component={ClassroomClimate}
+                        />
+                        <PrivateRoute
+                            auth={this.state.auth}
+                            path="/Magic8Menu"
+                            component={Magic8Menu}
+                        />
+                        <Route render={() => <h3>No Match</h3>} />
+                    </Switch>
+                </MuiThemeProvider>
+            </BrowserRouter>
+        );
     }
 }
 
