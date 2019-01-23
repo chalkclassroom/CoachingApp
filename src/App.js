9dbee0d96443f978e8c5a7be9c8e4232c1091fa7
Amanda Brandeen <amanda.l.brandeen@vanderbilt.edu>
2019-01-23 13:57:34 -0600

    made page and app bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import WelcomePage from "./views/WelcomeViews/Welcome";
 import ClassroomClimate from "./views/protected/ClassroomClimateViews/ClassroomClimate";
 import Magic8Menu from "./views/protected/Magic8Menu";
+import TransitionResults from "./views/protected/TransitionViews/TransitionResults";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import TransitionTime from "./views/protected/TransitionViews/TransitionTime";
 import ForgotPasswordPage from "./views/ForgotPasswordViews/ForgotPassword";
@@ -139,6 +140,11 @@ class App extends Component {
                             auth={this.state.auth}
                             path="/Magic8Menu"
                             component={Magic8Menu}
+                        />
+                        <PrivateRoute
+                            auth={this.state.auth}
+                            path="/TransitionResults"
+                            component={TransitionResults}
                         />
                         <Route render={() => <h3>No Match</h3>} />
                     </Switch>
---
+++ src/views/protected/TransitionViews/TransitionResults.js
@@ -0,0 +1,189 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Toolbar from "@material-ui/core/Toolbar";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button/Button";
+import InfoIcon from "@material-ui/icons/Help";
+import EditIcon from "@material-ui/icons/Edit";
+import {
+    createMuiTheme,
+    MuiThemeProvider,
+    withStyles
+} from "@material-ui/core/styles";
+import TransitionTimeHelp from "./TransitionTimeHelp";
+import TranstionType from "./TransitionType";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import TransitionTimer from "./TransitionTimer";
+import TransitionLog from "./TransitionLog";
+import YesNoDialog from "../../../components/Shared/YesNoDialog";
+import spreadsheetData from "../../../SPREADSHEET_SECRETS";
+import FirebaseContext from "../../../components/Firebase/context";
+import AppBar from "../../../components/AppBar";
+import { ImmortalDB } from "immortal-db";
+import cyan from "@material-ui/core/colors/teal";
+
+const styles = {
+    root: {
+        flexGrow: 1,
+        display: "flex",
+        minHeight: "100vh",
+        flexDirection: "column"
+    },
+    grow: {
+        flexGrow: 1
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20
+    }
+};
+
+const COLOR_1 = "#F9A796";
+const COLOR_2 = "#FFE79D";
+const COLOR_3 = "#4DEDBC";
+
+let getHexFromType = type => {
+    switch (type) {
+        case "Wait Time":
+            return COLOR_1;
+        case "Inside Classroom":
+            return COLOR_2;
+        case "Outside Classroom":
+            return COLOR_3;
+        default:
+            return "#FFFFFF";
+    }
+};
+
+class TransitionResults extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAppend = this.handleAppend.bind(this);
+        this.handleTypeChange = this.handleTypeChange.bind(this);
+    }
+
+    state = {
+        auth: true,
+        anchorEl: null,
+        help: false,
+        type: null,
+        hex: "#FFFFFF",
+        entries: [],
+        dbCounter: 0 // @Hack @Temporary !!!
+    };
+
+    componentDidMount() {
+        console.log(this.props.location.state);
+    }
+
+    handleAppend(entry) {
+        let newEntries = this.state.entries;
+        entry.type = this.state.type;
+        newEntries.push(entry);
+        this.setState({ entries: newEntries });
+
+        this.handleSpreadsheetAppend(entry);
+
+        this.handleDBinsert(entry);
+    }
+
+    handleTypeChange(newType) {
+        this.setState({ type: newType });
+        this.changeHex(newType);
+    }
+
+    changeHex = (type) => {
+        let mHex = getHexFromType(type);
+        this.setState({hex: mHex})
+    };
+
+    handleChange = event => {
+        this.setState({ auth: event.target.checked });
+    };
+
+    handleMenu = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+    handleHelpModal = () => {
+        this.setState({ help: true });
+    };
+
+    handleClickAway = () => {
+        this.setState({ help: false });
+    };
+
+    handleDBinsert = async entry => {
+        // Once we integrate users, the user + some index will be the key for the DB.
+        await ImmortalDB.set(
+            JSON.stringify(this.state.dbCounter),
+            JSON.stringify(entry)
+        );
+
+        this.setState({ dbCounter: this.state.dbCounter + 1 });
+    };
+
+    handleSpreadsheetAppend = entry => {
+        let url = new URL(spreadsheetData.scriptLink),
+            params = {
+                sheet: "TransitionTime",
+                del: "false",
+                TrnStart: entry.start,
+                TrnEnd: entry.end,
+                TrnDur: entry.duration,
+                TrnType: entry.type,
+                TeacherID: this.props.location.state.key.id
+            };
+        Object.keys(params).forEach(key =>
+            url.searchParams.append(key, params[key])
+        );
+        fetch(url, {
+            method: "POST",
+            credentials: "include",
+            mode: "no-cors",
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then(response => console.log("Success"))
+            .catch(error => console.error("Error:", error));
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { auth, anchorEl } = this.state;
+        const open = Boolean(anchorEl);
+
+        return (
+            <div className={classes.root}>
+                <FirebaseContext.Consumer>
+                    {firebase => <AppBar firebase={firebase} />}
+                </FirebaseContext.Consumer>
+                <main style={{ flex: 1 }}>
+                    <Grid container spacing={16}>
+                    </Grid>
+                </main>
+            </div>
+        );
+    }
+}
+
+TransitionResults.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(TransitionResults);
