0c7ad9193e372649703605df93acd64f1aa7015c
clarah2 <clare.speer@gmail.com>
2019-07-19 10:12:21 -0500

    added Transition Look-Fors

--- src/views/protected/TransitionViews/TransitionTimeHelp.js
+++ src/views/protected/TransitionViews/TransitionTimeHelp.js
@@ -63,83 +63,179 @@ class TransitionTimeHelp extends React.Component {
                 which <strong>most</strong> of the class is not involved in a
                 learning activity.
               </Typography>
-              <Typography variant="h6" gutterBottom>
-                Hints + Reminders: Classifying Transition Types
-              </Typography>
               <Table>
                 <TableHead>
                   <TableRow>
                     <TableCell
                       style={{
-                        backgroundColor: "#E69129",
+                        backgroundColor: "#094492",
                         color: "white",
                         fontSize: 18,
                         textAlign: "center",
                         width: "50%"
                       }}
                     >
-                      INSIDE
+                      TRANSITIONS BEGIN WHEN:
                     </TableCell>
                     <TableCell
                       style={{
-                        backgroundColor: "#46D3AA",
+                        backgroundColor: "#094492",
                         color: "white",
                         fontSize: 18,
                         textAlign: "center",
                         width: "50%"
                       }}
                     >
-                      OUTSIDE
+                      TRANSITIONS END WHEN:
                     </TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   <TableRow>
-                    <TableCell style={{ backgroundColor: "#FAE8CF" }}>
+                    <TableCell style={{ backgroundColor: "#759fe5", width: "50%"}}>
                       <strong>
-                        Children are waiting for a learning activity to
-                        begin/continue -OR- Children are participating in
-                        classroom routines
+                        A majority (more than half) of the children
+                        are in transition
                       </strong>
                     </TableCell>
-                    <TableCell style={{ backgroundColor: "#D7F6EE" }}>
+                    <TableCell style={{ backgroundColor: "#759fe5", width: "50%" }}>
                       <strong>
-                        Children are travelling to another area of the school
+                        A majority (more than half) of the children
+                        have started the next activity
                       </strong>
                     </TableCell>
                   </TableRow>
+                </TableBody>
+              </Table>
+              <Typography variant="subtitle2" gutterBottom style={{padding:10}}>
+              While you are recording a transition, choose the button representing 
+              the <strong>primary</strong> reason for that transition. <strong>Change </strong>  
+              the button if the primary reason changes during the same transition. 
+              Choose the <strong>“Other”</strong> button if the reason for the transition is not represented 
+              in the other buttons, and you will be prompted to explain in the Notes.
+              </Typography>
+              <Table>
+                <TableHead>
                   <TableRow>
-                    <TableCell style={{ backgroundColor: "#FAE8CF" }}>
-                      Cleaning up, handwashing, getting out cots or meal trays
+                    <TableCell padding="checkbox"
+                      style={{ 
+                        backgroundColor: "#094492", 
+                        color: "white",
+                        fontSize: 14,
+                        textAlign: "center", 
+                        width: "20%"}}>
+                      Waiting in line/lining up
                     </TableCell>
-                    <TableCell style={{ backgroundColor: "#D7F6EE" }}>
-                      Walking in the hallway
+                    <TableCell padding="checkbox"
+                      style={{ 
+                        backgroundColor: "#094492", 
+                        color: "white",
+                        fontSize: 14,
+                        textAlign: "center", 
+                        width: "20%"}}>
+                      Traveling outside the classroom
+                    </TableCell>
+                    <TableCell padding="checkbox"
+                      style={{ 
+                        backgroundColor: "#094492", 
+                        color: "white",
+                        fontSize: 14,
+                        textAlign: "center", 
+                        width: "20%"}}>
+                      Children waiting on teacher/materials
+                    </TableCell>
+                    <TableCell padding="checkbox"
+                      style={{ 
+                        backgroundColor: "#094492", 
+                        color: "white",
+                        fontSize: 14,
+                        textAlign: "center", 
+                        width: "20%"}}>
+                      Classroom Routines
+                    </TableCell>
+                    <TableCell padding="checkbox"
+                      style={{ 
+                        backgroundColor: "#094492", 
+                        color: "white",
+                        fontSize: 14,
+                        textAlign: "center", 
+                        width: "20%"}}>
+                      Behavior Management Disruption
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#AED581", width:"20%"}}>
+                      <strong>Lining up or waiting in line</strong>
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FFA726", width:"20%" }}>
+                      <strong>Walking from one part of the school to another</strong>
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FF7043", width:"20%" }}>
+                      <strong>Delays or interruptions because teacher or materials are not ready</strong>
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#64B5F6", width:"20%" }}>
+                      <strong>Participating in routine, non-learning activities</strong>
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FF5252", width:"20%" }}>
+                      <strong>Delays or interruptions due to behavior management</strong>
                     </TableCell>
                   </TableRow>
                   <TableRow>
-                    <TableCell style={{ backgroundColor: "#FAE8CF" }}>
-                      <strong>Children are waiting </strong> for instructions or
-                      materials
+                    <TableCell colSpan={5} style={{backgroundColor: "#759fe5", textAlign:"center"}}>
+                      <strong>EXAMPLES:</strong>
                     </TableCell>
-                    <TableCell style={{ backgroundColor: "#D7F6EE" }}>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#AED581", width:"20%"  }}>
                       Lining up to leave the classroom, playground, etc.
                     </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FFA726", width:"20%"  }}>
+                      Walking to the playground, library, music room, etc.
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FF7043", width:"20%" }}>
+                      Teacher stops an activity or delays the start of a new activity to gather or prepare materials 
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#64B5F6", width:"20%" }}>
+                      Cleaning up, hand-washing, getting out cots or meal trays, etc.
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FF5252", width:"20%" }}>
+                      Teacher stops a learning activity to address behavior
+                    </TableCell>
                   </TableRow>
                   <TableRow>
-                    <TableCell style={{ backgroundColor: "#FAE8CF" }}>
-                      Teacher <strong>stops an activity</strong> to gather
-                      materials or correct behavior
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#AED581", width:"20%"  }}>
+                      Children are lined up but are waiting to go to the next place
                     </TableCell>
-                    <TableCell style={{ backgroundColor: "#D7F6EE" }}>
-                      Bathroom and/or water break in the hallway
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FFA726", width:"20%"  }}>
+                      
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FF7043", width:"20%" }}>
+                      Teacher stops an activity or delays the start of a new activity to do something unrelated to activity
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#64B5F6", width:"20%" }}>
+                      Bathroom and/or water break in the classroom or hallway
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FF5252", width:"20%" }}>
+                     
                     </TableCell>
                   </TableRow>
                   <TableRow>
-                    <TableCell style={{ backgroundColor: "#FAE8CF" }}>
-                      One activity has ended but another hasn't started yet
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#AED581" }}>
+                      Waiting in line for lunch
                     </TableCell>
-                    <TableCell style={{ backgroundColor: "#D7F6EE" }}>
-                      Waiting in the lunch line
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FFA726" }}>
+                      
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FF7043"}}>
+                      
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#64B5F6"}}>
+                      Moving from one activity to another (e.g., whole group to centers)
+                    </TableCell>
+                    <TableCell padding="checkbox" style={{ backgroundColor: "#FF5252"}}>
+                      
                     </TableCell>
                   </TableRow>
                 </TableBody>
