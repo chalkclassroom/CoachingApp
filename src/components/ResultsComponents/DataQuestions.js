f3e0ca545692f67df7034e0c2268976ede8cc301
clarah2 <clare.speer@gmail.com>
2019-08-28 15:23:23 -0500

    cleaned code

--- src/components/ResultsComponents/DataQuestions.js
+++ src/components/ResultsComponents/DataQuestions.js
@@ -12,65 +12,63 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
 const styles = {
-    expansionPanel: {
-        overflow: "hidden"
-      },
-      expansionPanelTitle: {
-        variant: "subtitle2",
-        fontWeight: "bold",
-      },
-      expansionPanelText: {
-        variant: "body2"
-      },
-      addButton: {
-        fill: "#094492"
-      },
+  expansionPanel: {
+    overflow: "hidden"
+  },
+  expansionPanelTitle: {
+    variant: "subtitle2",
+    fontWeight: "bold",
+  },
+  expansionPanelText: {
+    variant: "body2"
+  },
+  addButton: {
+    fill: "#094492"
+  },
 };
 
 class DataQuestions extends React.Component {
-
-    constructor(props){
-        super(props);
-    }
-
-    render() {
-        const { classes } = this.props;
-        return(
-            <div>
-                {this.props.questions.map((item, index) => (
-                    <ExpansionPanel
-                        key={index}
-                        className={classes.expansionPanel}
-                        expanded={this.props.openPanel===item.name}
-                        onChange={this.props.handlePanelChange.bind(this, item.name)}
-                    >
-                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                            <Typography
-                                className={classes.expansionPanelTitle}
-                                style={{textDecoration: this.props.addedToPrep.includes(item.name) ? "underline" : null}}
-                            >
-                                {item.title}
-                            </Typography>
-                        </ExpansionPanelSummary>
-                        <ExpansionPanelDetails>
-                            <Grid container direction="row">
-                                <Grid item xs={11}>
-                                    <Typography className={classes.expansionPanelText}>
-                                        {item.text}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={1}>
-                                    <Button onClick={this.props.handleAddToPlan.bind(this, item.name)}>
-                                        <AddCircleIcon className={classes.addButton}/>
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </ExpansionPanelDetails>
-                    </ExpansionPanel>
-                ))}
-            </div>
-        );
-    }
+  constructor(props){
+    super(props);
+  }
+  render() {
+    const { classes } = this.props;
+    return(
+      <div>
+        {this.props.questions.map((item, index) => (
+          <ExpansionPanel
+            key={index}
+            className={classes.expansionPanel}
+            expanded={this.props.openPanel===item.name}
+            onChange={this.props.handlePanelChange.bind(this, item.name)}
+          >
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography
+                className={classes.expansionPanelTitle}
+                style={{textDecoration: this.props.addedToPrep.includes(item.name) ? "underline" : null}}
+              >
+                {item.title}
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Grid container direction="row">
+                <Grid item xs={11}>
+                  <Typography className={classes.expansionPanelText}>
+                    {item.text}
+                  </Typography>
+                </Grid>
+                <Grid item xs={1}>
+                  <Button onClick={this.props.handleAddToPlan.bind(this, item.name)}>
+                    <AddCircleIcon className={classes.addButton}/>
+                  </Button>
+                </Grid>
+              </Grid>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        ))}
+      </div>
+    );
+  }
 }
 
 DataQuestions.propTypes = {
