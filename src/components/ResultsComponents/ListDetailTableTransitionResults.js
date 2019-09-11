28ed47b7cd653b63c087ba67b5a5a596b308b56d
clarah2 <clare.speer@gmail.com>
2019-09-11 14:35:49 -0500

    cleaned code

--- src/components/ResultsComponents/ListDetailTableTransitionResults.js
+++ src/components/ResultsComponents/ListDetailTableTransitionResults.js
@@ -9,93 +9,83 @@ import TableBody from "@material-ui/core/TableBody/TableBody";
 import Paper from "@material-ui/core/Paper/Paper";
 
 const styles = {
-    //idk how this works
+  paper: {
+    width: "60vw",
+    overflowX: "auto"
+  },
+  tableHeader: {
+    backgroundColor: "#3f51b5",
+    color: "white",
+    fontSize: 14
+  },
+  tableBody: {
+    backgroundColor: "#ffc107",
+    color: "black",
+    fontSize: 14
+  }
 };
 class ListDetailTableTransitionResults extends React.Component {
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
-        return (
-            <Paper
-                style={{ width: "60vw", overflowX: "auto"}}
-            >
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell
-                                style={{
-                                    backgroundColor: "#3f51b5",
-                                    color: "white",
-                                    fontSize: 14
-                                }}
-                            >
-                                Start Time
-                            </TableCell>
-                            <TableCell
-                                style={{
-                                    backgroundColor: "#3f51b5",
-                                    color: "white",
-                                    fontSize: 14
-                                }}
-                                align="right"
-                            >
-                                Duration
-                            </TableCell>
-                            <TableCell
-                                style={{
-                                    backgroundColor: "#3f51b5",
-                                    color: "white",
-                                    fontSize: 14
-                                }}
-                                align="right"
-                            >
-                                Type
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {this.props.data.map(row => (
-                            <TableRow className={classes.row} key={row.id}>
-                                <TableCell component="th" scope="row">
-                                    {row.startTime}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {row.duration}
-                                </TableCell>
-
-                                {row.type === "INSIDE" ? (
-                                    <TableCell
-                                        style={{
-                                            backgroundColor: "#ffc107",
-                                            color: "black",
-                                            fontSize: 14
-                                        }}
-                                    >
-                                        {row.type}
-                                    </TableCell>
-                                ) : row.type === "OUTSIDE" ? (
-                                    <TableCell
-                                        style={{
-                                            backgroundColor: "#f44336",
-                                            color: "black",
-                                            fontSize: 14
-                                        }}
-                                    >
-                                        {row.type}
-                                    </TableCell>
-                                )  : null}
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Paper>
-        );
-    }
+    return (
+      <Paper className={classes.paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell
+                className={classes.tableHeader}
+              >
+                Start Time
+              </TableCell>
+              <TableCell
+                className={classes.tableHeader}
+                align="right"
+              >
+                Duration
+              </TableCell>
+              <TableCell
+                className={classes.tableHeader}
+                align="right"
+              >
+                Type
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.props.data.map(row => (
+              <TableRow className={classes.row} key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.startTime}
+                </TableCell>
+                <TableCell align="right">
+                  {row.duration}
+                </TableCell>
+                {row.type === "INSIDE" ? (
+                  <TableCell
+                    className={classes.tableBody}
+                  >
+                    {row.type}
+                  </TableCell>
+                ) : row.type === "OUTSIDE" ? (
+                  <TableCell
+                    className={classes.tableBody}
+                  >
+                    {row.type}
+                  </TableCell>
+                ) : null}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 ListDetailTableTransitionResults.propTypes = {
-    classes: PropTypes.object.isRequired,
-    data: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(ListDetailTableTransitionResults);
--- src/components/ResultsComponents/NotesListDetailTable.js
+++ src/components/ResultsComponents/NotesListDetailTable.js
@@ -9,58 +9,56 @@ import TableBody from "@material-ui/core/TableBody/TableBody";
 import Paper from "@material-ui/core/Paper/Paper";
 
 const styles = {
+  paper: {
+    width: "100%",
+    overflowX: "auto",
+    marginRight: "10%"
+  },
+  tableHeader: {
+    backgroundColor: "#2196f3",
+    color: "white",
+    fontSize: 14
+  },
 };
-class NotesListDetailTable extends React.Component {
-    render() {
-        const { classes } = this.props;
 
-        return (
-            <Paper
-                style={{ width: "100%", overflowX: "auto", marginRight: "10%" }}
-            >
-                <Table className={classes.table}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell
-                                style={{
-                                    backgroundColor: "#2196f3",
-                                    color: "white",
-                                    fontSize: 14
-                                }}
-                            >
-                                Time
-                            </TableCell>
-                            <TableCell
-                                style={{
-                                    backgroundColor: "#2196f3",
-                                    color: "white",
-                                    fontSize: 14
-                                }}
-                                align="right"
-                            >
-                                Notes
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {this.props.data.map(row => (
-                            <TableRow className={classes.row} key={row.id}>
-                                <TableCell component="th" scope="row">
-                                    {row.timestamp}
-                                </TableCell>
-                                <TableCell align="right">{row.content}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Paper>
-        );
-    }
+class NotesListDetailTable extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Paper className={classes.paper}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.tableHeader}>
+                Time
+              </TableCell>
+              <TableCell className={classes.tableHeader} align="right">
+                Notes
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.props.data.map(row => (
+              <TableRow className={classes.row} key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.timestamp}
+                </TableCell>
+                <TableCell align="right">
+                  {row.content}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 NotesListDetailTable.propTypes = {
-    classes: PropTypes.object.isRequired,
-    data: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(NotesListDetailTable);
