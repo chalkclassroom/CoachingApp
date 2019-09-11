99d792bc78eab0c7647ecb76fc36e50e8e223e1b
clarah2 <clare.speer@gmail.com>
2019-09-11 13:16:00 -0500

    cleaned code

--- src/views/ForgotPasswordViews/ForgotPassword.js
+++ src/views/ForgotPasswordViews/ForgotPassword.js
@@ -6,35 +6,36 @@ import FirebaseContext from "../../components/Firebase/context";
 import ResetForm from "./ResetForm"
 
 const styles = {
-    root: {
-        flexGrow: 1,
-        backgroundColor:'#2196f3',
-        height: '100vh'
-    },
-    grow: {
-        flexGrow: 1,
-    }
+  root: {
+    flexGrow: 1,
+    backgroundColor:'#2196f3',
+    height: '100vh'
+  },
+  grow: {
+    flexGrow: 1,
+  }
 };
 
 class Welcome extends React.Component {
-
-    render() {
-        const {classes} = this.props;
-        return (
-            <div className={classes.root}>
-                <FirebaseContext.Consumer>
-                    {
-                        firebase => <div><AppBar firebase={firebase}/> <ResetForm firebase={firebase}/></div>
-                    }
-                </FirebaseContext.Consumer>
-
+  render() {
+    const {classes} = this.props;
+    return (
+      <div className={classes.root}>
+        <FirebaseContext.Consumer>
+          {firebase =>
+            <div>
+              <AppBar firebase={firebase}/>
+              <ResetForm firebase={firebase}/>
             </div>
-        );
-    }
+          }
+        </FirebaseContext.Consumer>
+      </div>
+    );
+  }
 }
 
 Welcome.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Welcome);
