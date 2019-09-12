114f97a506d0bd8d51d9caaee86baa212fd62b80
clarah2 <clare.speer@gmail.com>
2019-09-12 09:25:22 -0500

    cleaned code

--- src/components/AssociativeCooperativeComponents/ResultsComponents/TeacherBehaviorsPie.js
+++ src/components/AssociativeCooperativeComponents/ResultsComponents/TeacherBehaviorsPie.js
@@ -4,7 +4,7 @@ import { withStyles } from "@material-ui/core";
 import { Pie } from "react-chartjs-2";
 
 const styles = {
-    //idk how this works
+
 };
 
 /**
@@ -29,70 +29,67 @@ const styles = {
 // };
 
 class TeacherBehaviorsPie extends React.Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props){
+    super(props);
+  }
 
-    state = {
+  state = {
 
+  };
 
+  render() {
+    const { classes } = this.props;
+    // console.log("inside time: ", this.state.inside);
+    console.log("total session time: " + this.props.sessionTotal)
+    let teacherBehaviorsData = {
+      labels: [
+        'Teacher Support for Assoc./Coop. Interactions',
+        'Teacher Present, No Support'
+      ],
+
+      datasets: [{
+        data: [this.props.noSupportTime, this.props.supportTime],
+        backgroundColor: [
+          '#0988EC',
+          '#E99C2E',
+        ],
+        hoverBackgroundColor: [
+          '#0988EC',
+          '#E99C2E',
+        ]
+      }]
     };
 
-    render() {
-        const { classes } = this.props;
-        // console.log("inside time: ", this.state.inside);
-        console.log("total session time: " + this.props.sessionTotal)
-        let teacherBehaviorsData = {
-            labels: [
-                'Teacher Support for Assoc./Coop. Interactions',
-                'Teacher Present, No Support'
-            ],
-
-            datasets: [{
-                data: [this.props.noSupportTime, this.props.supportTime],
-                backgroundColor: [
-                    '#0988EC',
-                    '#E99C2E',
-                ],
-                hoverBackgroundColor: [
-                    '#0988EC',
-                    '#E99C2E',
-                ]
-
-            }]
-
-
-        };
-
-
-        return (
-            <Pie data={teacherBehaviorsData} options={{
-                tooltips: {
-                    callbacks: {
-                        label: function(tooltipItem, data) {
-                            var dataset = data.datasets[tooltipItem.datasetIndex];
-                            var meta = dataset._meta[Object.keys(dataset._meta)[0]];
-                            var total = meta.total;
-                            var currentValue = dataset.data[tooltipItem.index];
-                            var percentage = parseFloat((currentValue/total*100).toFixed(1));
-                            return currentValue + ' (' + percentage + '%)';
-                        },
-                        title: function(tooltipItem, data) {
-                            return data.labels[tooltipItem[0].index];
-                        }
-                    }
-                }
-            }}
-                 width="650"
-                 height="400"
-            />
-        );
-    }
+    return (
+      <Pie 
+        data={teacherBehaviorsData}
+        options={{
+          tooltips: {
+            callbacks: {
+              label: function(tooltipItem, data) {
+                var dataset = data.datasets[tooltipItem.datasetIndex];
+                var meta = dataset._meta[Object.keys(dataset._meta)[0]];
+                var total = meta.total;
+                var currentValue = dataset.data[tooltipItem.index];
+                var percentage = parseFloat((currentValue/total*100).toFixed(1));
+                return currentValue + ' (' + percentage + '%)';
+              },
+              title: function(tooltipItem, data) {
+                return data.labels[tooltipItem[0].index];
+              }
+            }
+          }
+        }}
+        width="650"
+        height="400"
+      />
+    );
+  }
 }
 
  TeacherBehaviorsPie.propTypes = {
-    classes: PropTypes.object.isRequired,
-    data: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(TeacherBehaviorsPie);
