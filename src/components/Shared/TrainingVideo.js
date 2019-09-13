7c24c2c1d291ba81eef7167a4a58bcd6479196bd
clarah2 <clare.speer@gmail.com>
2019-09-13 10:09:07 -0500

    cleaned code

--- src/components/Shared/TrainingVideo.js
+++ src/components/Shared/TrainingVideo.js
@@ -5,23 +5,33 @@ import PropTypes from "prop-types";
 
 class TrainingVideo extends React.Component {
 
-render() {
+  render() {
     return (
-        <Video autoPlay loop muted
-               controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
-               poster="http://sourceposter.jpg"
-               onCanPlayThrough={() => {
-                   // Do stuff
-               }}>
-            <source src={this.props.videoUrl} type="video/mp4" />
-            <track label="English" kind="subtitles" srcLang="en" src="http://source.vtt" default />
-        </Video>
+      <Video 
+        autoPlay
+        loop
+        muted
+        controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
+        poster="http://sourceposter.jpg"
+        onCanPlayThrough={() => {
+          // Do stuff
+        }}
+      >
+        <source src={this.props.videoUrl} type="video/mp4" />
+        <track
+          label="English"
+          kind="subtitles"
+          srcLang="en"
+          src="http://source.vtt"
+          default
+        />
+      </Video>
     );
-}
+  }
 }
 
 TrainingVideo.propTypes = {
-    videoUrl: PropTypes.string.isRequired
+  videoUrl: PropTypes.string.isRequired
 };
 
 export default TrainingVideo;
