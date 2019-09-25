405ecab07a229bb3bd19e907e28493145ff9002d
clarah2 <clare.speer@gmail.com>
2019-09-25 15:13:16 -0500

    Revert "Merge branch 'master' of https://github.com/CQ-REF/CoachingApp"
    This reverts commit 92ce75632fba8a782609bb282b38320b33b21a5f, reversing
    changes made to 2a0a1c3c384036d4b6cfe92fd282236b387ad49b.
    
    e reverts last commit

---
+++ public/precache-manifest.a9fa961c46393521d9f8ca32150fa1ba.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "674ab0168fa00450da5fc456ae3361bd",
+    "url": "/static/media/LevelofInstruction.674ab016.svg"
+  },
+  {
+    "revision": "2c6c86842e06142ca6c6",
+    "url": "/static/css/main.f95e6416.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "75793d24e733b50a735157edcebda93a",
+    "url": "/static/media/messages.75793d24.svg"
+  },
+  {
+    "revision": "a6c40752c96eff6402ae",
+    "url": "/static/js/2.a6c40752.chunk.js"
+  },
+  {
+    "revision": "aac7b188bc2194bd16cc1b1ea31f0c36",
+    "url": "/static/media/1-ex-negative-cqref.aac7b188.png"
+  },
+  {
+    "revision": "4411c87a7f12b5111c530cc8dadd5f77",
+    "url": "/static/media/2-negative-cqref.4411c87a.png"
+  },
+  {
+    "revision": "38d103c1db6bd506ce2fac8e3bdbe7e6",
+    "url": "/static/media/3-flat-cqref.38d103c1.png"
+  },
+  {
+    "revision": "e62516c4f277de0f8435b87e8257b516",
+    "url": "/static/media/4-pleasant-cqref.e62516c4.png"
+  },
+  {
+    "revision": "4d2e48e6757c250a5a23ac2a6700872d",
+    "url": "/static/media/5-vibrant-cqref.4d2e48e6.png"
+  },
+  {
+    "revision": "5e0a62e481899a9c33209f42d0259a39",
+    "url": "/static/media/ClassroomClimate.5e0a62e4.svg"
+  },
+  {
+    "revision": "5546273171186a17ad66fc50def2622c",
+    "url": "/static/media/TransitionTime.55462731.svg"
+  },
+  {
+    "revision": "bd138af5f8c1dbc575f5526730cb7668",
+    "url": "/static/media/logo.bd138af5.svg"
+  },
+  {
+    "revision": "99c5f813f017b8c5f0da8c7f916eebbc",
+    "url": "/static/media/slide1.99c5f813.png"
+  },
+  {
+    "revision": "7ede90f656405ddd34024eaafa0a099d",
+    "url": "/static/media/slide2.7ede90f6.png"
+  },
+  {
+    "revision": "388464eace239ea9b39a024bd6271d1d",
+    "url": "/static/media/slide3.388464ea.png"
+  },
+  {
+    "revision": "d76374551e3c5ed82c1e58a2f4646389",
+    "url": "/static/media/slide4.d7637455.png"
+  },
+  {
+    "revision": "3769d43387543cef14f85427bf02e767",
+    "url": "/static/media/AssocCoopInteractions.3769d433.svg"
+  },
+  {
+    "revision": "2c6c86842e06142ca6c6",
+    "url": "/static/js/main.2c6c8684.chunk.js"
+  },
+  {
+    "revision": "0f6f1652c06230614ea118d05689ec25",
+    "url": "/static/media/ListeningtoChildren.0f6f1652.svg"
+  },
+  {
+    "revision": "cf0d310f1aa86fd489b21fe4578a3f4b",
+    "url": "/static/media/MathInstruction.cf0d310f.svg"
+  },
+  {
+    "revision": "5254a9c0cc3946759e67231aed22757e",
+    "url": "/static/media/SequentialActivities.5254a9c0.svg"
+  },
+  {
+    "revision": "a2f5344dad1b63be513a24151036f407",
+    "url": "/static/media/StudentEngagement.a2f5344d.svg"
+  },
+  {
+    "revision": "4af2240f5169e3d534cf93520de0f69d",
+    "url": "/static/media/teacher.4af2240f.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "44ce6aee6a3d69f3cd14e8f62d12192e",
+    "url": "/static/media/coach.44ce6aee.svg"
+  },
+  {
+    "revision": "d3a210d5307a6ab00dde37516c5e3d52",
+    "url": "/static/media/manager.d3a210d5.svg"
+  },
+  {
+    "revision": "2095c29829b26a891516723a8c464d1a",
+    "url": "/static/media/generateReport.2095c298.svg"
+  },
+  {
+    "revision": "680adf8a8879a038045a06d85cef6b66",
+    "url": "/static/media/calendar.680adf8a.svg"
+  },
+  {
+    "revision": "aec0da1d7109660fdb92f0e9b7ad0da3",
+    "url": "/static/media/observe.aec0da1d.svg"
+  },
+  {
+    "revision": "fa6310220fb21ec6bf958113b3a06be4",
+    "url": "/static/media/results.fa631022.svg"
+  },
+  {
+    "revision": "a6c40752c96eff6402ae",
+    "url": "/static/css/2.471c85d1.chunk.css"
+  },
+  {
+    "revision": "19e1eb2a7193b082bbcc40f6db3aebd4",
+    "url": "/index.html"
+  }
+];
