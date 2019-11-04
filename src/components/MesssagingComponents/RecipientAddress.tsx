6d3347946b1bdec0ced86d187892cb85473ec129
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-11-04 17:25:04 -0600

    basic template for recipient field

--- src/components/MesssagingComponents/RecipientAddress.tsx
+++ src/components/MesssagingComponents/RecipientAddress.tsx
@@ -1,2 +1,22 @@
 // A simple To: bar to input email address of the recipient
 // Connected to database to show the list of the teachers the user can send to
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+export default class RecipentAddress extends React.Component<{}, {}> {
+  render() {
+    return (
+        <form noValidate autoComplete="off">
+        <div>
+            <TextField
+                id="outlined-basic"
+                label="Outlined"
+                margin="normal"
+                variant="outlined"
+                style={{width: "100%"}}
+            />
+        </div>
+        </form>
+    );
+  }
+}
