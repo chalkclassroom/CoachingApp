a12138c0cdd4b04d93bb2c735a2f542d10738027
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-11-04 17:06:27 -0600

    added base template for submit button

--- src/components/MesssagingComponents/SubmitButton.tsx
+++ src/components/MesssagingComponents/SubmitButton.tsx
@@ -1,1 +1,13 @@
 // A simple submit button which which submits the form
+import React from 'react';
+import { Fab } from '@material-ui/core';
+
+export class SubmitButton extends React.Component<{}, {}> {
+    render() {
+        return(
+            <Fab variant="extended" aria-label="like">
+                Submit
+            </Fab>
+        );
+    }
+}
