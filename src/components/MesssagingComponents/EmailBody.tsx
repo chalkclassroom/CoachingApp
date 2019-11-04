ab555e82ac5415c05ff85bf6546e60a3a9f29dab
Andrew Huang <andrewhuang2000@gmail.com>
2019-11-04 15:45:24 -0600

    import added

--- src/components/MesssagingComponents/EmailBody.tsx
+++ src/components/MesssagingComponents/EmailBody.tsx
@@ -1,2 +1,5 @@
 // Shows the rendered HTML output depending on what the used chose from the ChooseIntent component.
 // Allows editing of the contents to customise the outgoing email.
+import React from 'react';
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
