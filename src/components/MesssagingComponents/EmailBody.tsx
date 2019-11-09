f13119366961e1e325568cea3cb37eb5c7163f32
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-11-09 02:06:52 -0600

    added contenteditable

--- src/components/MesssagingComponents/EmailBody.tsx
+++ src/components/MesssagingComponents/EmailBody.tsx
@@ -1,24 +1,34 @@
 // Shows the rendered HTML output depending on what the used chose from the ChooseIntent component.
 // Allows editing of the contents to customise the outgoing email.
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 
-const EmailBody: React.FC<{ intent: string }> = (props) => {
+const EmailBody: React.FC<{ intent: string; text: string }> = (props) => {
+    const textVal = useRef<HTMLParagraphElement>(null);
+
+    const handleClick = () => {
+        if (textVal && textVal.current){
+            let emailText = textVal.current.textContent;
+            // TODO best approach?
+            props.text = emailText !== null ? emailText : props.text;
+        }
+    }
+
     const renderBody = () => {
         const intent = props.intent;
         if (intent === "") {
-            return <h1>Choose intent</h1>;
+            return <h1 contentEditable={true} ref={textVal} onClick={handleClick}>Choose intent</h1>;
         } else if (intent === "custom") {
-            return <h2>Woohoo! Custom content!</h2>;
+            return <h2 contentEditable={true}>Woohoo! Custom content!</h2>;
         } else if (intent === "thank_you") {
-            return <h2>Thank you!</h2>;
+            return <h2 contentEditable={true}>Thank you!</h2>;
         } else if (intent === "action_plan") {
-            return <h2>Action Plan!</h2>;
+            return <h2 contentEditable={true}>Action Plan!</h2>;
         } else if (intent === "schedule_meeting") {
-            return <h2>Let's meet!</h2>;
+            return <h2 contentEditable={true}>Let's meet!</h2>;
         } else {
-            return <h2>WTF</h2>;
+            return <h2 contentEditable={true}>WTF</h2>;
         }
     }
     
