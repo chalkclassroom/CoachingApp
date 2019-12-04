8480ac553021feedc855fafd62a6027a74bfd4af
Sri Harsha <harsha@fortyau.com>
2019-12-04 02:04:46 -0600

    workbox complete

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,12 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+if ('serviceWorker' in navigator) {
+   window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+       console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+     });
+   });
+}
