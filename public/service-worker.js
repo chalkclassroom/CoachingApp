a1af1c46ce2dc0e925eae5d52ffb6d2087362869
Sri Harsha <sriharrsha@users.noreply.github.com>
2019-12-04 19:57:03 -0600

    site.manifests updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c1338b70a92595c0682b24e78fbd3d9.js"
+  "/precache-manifest.a7516ec995916857f4bda17eaeb8bf49.js"
 );
 
 workbox.core.skipWaiting();
