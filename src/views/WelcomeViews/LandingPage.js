0805ed35395e6b493d21a33ceda68eec7d50456d
clarah2 <clare.speer@gmail.com>
2019-12-08 19:46:16 -0600

    add global site tag

--- src/views/WelcomeViews/LandingPage.js
+++ src/views/WelcomeViews/LandingPage.js
@@ -276,6 +276,14 @@ class LandingPage extends React.Component {
         <head>
           <link href="https://fonts.googleapis.com/css?family=Arimo&display=swap" rel="stylesheet" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-S797QZ8L3N"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)};
+            gtag('js', new Date());
+
+            gtag('config', 'G-S797QZ8L3N');
+          </script>
         </head>
         <body className={classes.root}>
           {this.state.events ? (
