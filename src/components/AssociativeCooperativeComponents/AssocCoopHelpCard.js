ffa519948701c2e25c28f66bdddf6f6dd3bffd33
vurajeev <58226973+vurajeev@users.noreply.github.com>
2020-01-20 13:48:02 -0600

    Req2: ACI Helpcard, fixed:  indentation/spacing

--- src/components/AssociativeCooperativeComponents/AssocCoopHelpCard.js
+++ src/components/AssociativeCooperativeComponents/AssocCoopHelpCard.js
@@ -37,15 +37,15 @@ const styles = () => ({
     textAlign: "center"
   },
   buttonTitle: {
-    backgroundColor: "#f6f6f1", 
+    backgroundColor: "#f6f6f1",
     color: "black",
     fontSize: 14,
-    textAlign: "center", 
+    textAlign: "center",
     width: "20%"
   },
   lineExamples: {
     backgroundColor: "#f6f6f1",
-    width:"20%"
+    width: "20%"
   }
 });
 
@@ -57,27 +57,27 @@ function AssocCoopHelpCard(props) {
         <TableHead  >
           <TableRow>
             <TableCell colSpan={4} className={classes.definitionTitle}>
-           Child Behaviors         
+              Child Behaviors
             </TableCell>
           </TableRow>
         </TableHead>
-      </Table> 
+      </Table>
 
       <Table>
-      <TableHead>
+        <TableHead>
           <TableRow>
             <TableCell colSpan={2} className={classes.definitionText}>
               <strong>
-              Associative Interactions </strong>
+                Associative Interactions </strong>
             </TableCell>
             <TableCell colSpan={2} className={classes.definition2Text}>
               <strong>
-              Cooperative Interactions
+                Cooperative Interactions
               </strong>
             </TableCell>
           </TableRow>
         </TableHead>
-      <TableBody>
+        <TableBody>
           <TableRow>
             <TableCell padding="checkbox" className={classes.buttonTitle}>
               <strong> Participating in a conversation about a shared activity</strong>
@@ -95,40 +95,40 @@ function AssocCoopHelpCard(props) {
 
           <TableRow>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Talking about what kind of structure to build at blocks
+              Talking about what kind of structure to build at blocks
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Pretending to be a family but it’s unclear who is the parent and who is the child; no defined storyline
+              Pretending to be a family but it’s unclear who is the parent and who is the child; no defined storyline
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Following the rules for a memory card game
+              Following the rules for a memory card game
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-             Pretending to be a customer and server at a restaurant while using role speech (“What would you like to eat?”)
+              Pretending to be a customer and server at a restaurant while using role speech (“What would you like to eat?”)
             </TableCell>
           </TableRow>
 
           <TableRow>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Talking while making pretend potions together at the sand table
+              Talking while making pretend potions together at the sand table
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples} >
-            Drawing together at the dry erase board
+              Drawing together at the dry erase board
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Following a set of rules for an invented game
+              Following a set of rules for an invented game
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Completing a pattern block design together
-            </TableCell>    
+              Completing a pattern block design together
+            </TableCell>
           </TableRow>
 
           <TableRow>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Talking to a teacher about a book character            
+              Talking to a teacher about a book character
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Moving cars around the blocks center together
+              Moving cars around the blocks center together
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
             </TableCell>
@@ -137,17 +137,17 @@ function AssocCoopHelpCard(props) {
           </TableRow>
         </TableBody>
       </Table>
-      
+
       <Table>
         <TableHead>
           <TableRow>
             <TableCell colSpan={4} className={classes.definition2Title}>
-           Teacher Behaviors         
+              Teacher Behaviors
             </TableCell>
           </TableRow>
         </TableHead>
 
-        <TableHead>      
+        <TableHead>
           <TableRow>
             <TableCell padding="checkbox" className={classes.buttonTitle}>
               <strong>Participating in children’s play </strong>
@@ -160,50 +160,50 @@ function AssocCoopHelpCard(props) {
             </TableCell>
             <TableCell padding="checkbox" className={classes.buttonTitle}>
               <strong>Helping children find the words to communicate </strong>
-              </TableCell>            
+            </TableCell>
           </TableRow>
-          </TableHead>
+        </TableHead>
 
         <TableBody>
           <TableRow>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Talking to children about their play-doh creations
+              Talking to children about their play-doh creations
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Asking children what will happen next during dramatic play
+              Asking children what will happen next during dramatic play
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Demonstrating how to play a new game
+              Demonstrating how to play a new game
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Prompting children to ask peers to play or share a material
+              Prompting children to ask peers to play or share a material
             </TableCell>
           </TableRow>
 
           <TableRow>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Co-writing a label with a child for his drawing
+              Co-writing a label with a child for his drawing
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples} >
-          Asking children what shape of blocks they will need to keep their building from falling
+              Asking children what shape of blocks they will need to keep their building from falling
              </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Modeling the steps for sharing
+              Modeling the steps for sharing
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Helping children solve problems during games or other shared activities (Say, “It’s my turn.”)
+              Helping children solve problems during games or other shared activities (Say, “It’s my turn.”)
             </TableCell>
           </TableRow>
 
           <TableRow>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Taking on a role in dramatic play
+              Taking on a role in dramatic play
            </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
-            Asking children what they think a character will do next while reading a book together
-            </TableCell>           
-            <TableCell  padding="checkbox" className={classes.lineExamples}>
-            Giving positive feedback when children share or interact
+              Asking children what they think a character will do next while reading a book together
+            </TableCell>
+            <TableCell padding="checkbox" className={classes.lineExamples}>
+              Giving positive feedback when children share or interact
             </TableCell>
             <TableCell padding="checkbox" className={classes.lineExamples}>
             </TableCell>
