12b02710d0b7d5ebf084d519d322f6eed9c07439
Andrew Huang <andrew.huang@vanderbilt.edu>
2020-01-22 16:52:59 -0600

    autocomplete

--- src/components/MesssagingComponents/RecipientAddress.tsx
+++ src/components/MesssagingComponents/RecipientAddress.tsx
@@ -2,21 +2,24 @@
 // Connected to database to show the list of the teachers the user can send to
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-
+import Autocomplete from '@material-ui/lab/Autocomplete';
+ 
 const RecipentAddress: React.FC<{}> = () => {
-  return (
-    <form noValidate autoComplete="off">
-      <div>
-        <TextField
-          id="outlined-basic"
-          label="Outlined"
-          margin="normal"
-          variant="outlined"
-          style={{width: "100%"}}
-        />
-      </div>
-    </form>
-  );
+ return (
+   <Autocomplete
+     options={top5}
+     getOptionLabel={option => option.title}
+     <div>
+       <TextField
+         id="outlined-basic"
+         label="Outlined"
+         margin="normal"
+         variant="outlined"
+         style={{width: "100%"}}
+       />
+     </div>
+   />
+ );
 }
-
+ 
 export default TextField;
