44ce4840fd90169c9dd07237f2b7e6d4e56fcb28
Mingli <charlie2014rha@gmail.com>
2020-01-22 17:14:24 -0600

    fix import

--- src/views/protected/MessagingViews/Messaging.tsx
+++ src/views/protected/MessagingViews/Messaging.tsx
@@ -4,6 +4,9 @@ import ChooseIntent from '../../../components/MesssagingComponents/ChooseIntent'
 import EmailBody from '../../../components/MesssagingComponents/EmailBody';
 import RecipientAddress from '../../../components/MesssagingComponents/RecipientAddress';
 import SubmitButton from '../../../components/MesssagingComponents/SubmitButton';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
