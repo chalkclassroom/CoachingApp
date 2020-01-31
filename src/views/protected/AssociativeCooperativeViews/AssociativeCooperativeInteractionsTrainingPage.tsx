e6cb0ab9f127c67e66b8e040947609556b50047d
vurajeev <58226973+vurajeev@users.noreply.github.com>
2020-01-30 23:51:17 -0600

    added ACI & Sequential  to the training dashboard

--- src/views/protected/AssociativeCooperativeViews/AssociativeCooperativeInteractionsTrainingPage.tsx
+++ src/views/protected/AssociativeCooperativeViews/AssociativeCooperativeInteractionsTrainingPage.tsx
@@ -1,56 +1,153 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
-import Grid from "@material-ui/core/Grid/index";
+import * as Constants from "../../../constants/index";
 import Button from "@material-ui/core/Button/Button";
-import List from "@material-ui/core/List/index";
-import ListItem from "@material-ui/core/ListItem/index";
 import AssocCoopIconImage from "../../../assets/images/AssocCoopIconImage.svg";
 import { withStyles } from "@material-ui/core/styles/index";
-import FirebaseContext from "../../../components/Firebase/FirebaseContext";
 import AppBar from "../../../components/AppBar";
-import Typography from "@material-ui/core/Typography/Typography";
+import FirebaseContext from "../../../components/Firebase/FirebaseContext";
 import "chartjs-plugin-datalabels";
-import TrainingVideo from "../../../components/Shared/TrainingVideo";
+import TrainingVideo from "../../../components/Shared/TrainingVideo.tsx";
 import TrainingQuestionnaire from "../../../components/Shared/TrainingQuestionnaire";
+import TrainingDashboard from '../../../components/Shared/TrainingDashboard';
+import AssocCoopHelpCard from '../../../components/AssociativeCooperativeComponents/AssocCoopHelpCard';
+import ChevronLeftRoundedIcon from '@material-ui/icons/ChevronLeftRounded';
+
 
 const styles: object = {
   root: {
     flexGrow: 1,
-    height: "100vh",
-    flexDirection: "column"
+    height: '100vh',
+    width: '100vw',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    overflowX: 'hidden',
+    overflowY: 'scroll',
+    margin: '0',
+    padding: '0'
   },
-  viewButtons: {
-    minWidth: 150,
-    textAlign: "center"
+  titleContainer: {
+    width: '100%',
+    margin: '0',
+    padding: '0',
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   },
-  buttonsList: {
-    position: "relative",
-    top: "3vh"
+  backButton: {
+    color: '#333333',
+    borderRadius: 3,
+    textTransform: 'none'
   },
-  resultsContent: {
-    height: "60vh",
-    position: "relative",
-    top: "8vh"
+  main: {
+    height: 'auto',
+    flexGrow: 1,
+    display: 'grid',
+    gridTemplateRows: '100%',
+    gridTemplateColumns: '25% 75%',
+    margin: '2% 5% 2% 5%'
+  },
+  trainingContentCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    padding: '0% 4% 3% 4%',
+    overflowY: 'scroll',
+    overflowX: 'hidden'
+  },
+
+  // iPad Pro 12.9" Landscape
+  '@media only screen and (max-width:1366px) and (min-height:800px) and (orientation:landscape)': {
+    root: {
+      fontSize: '1.5em'
+    },
+    main: {
+      margin: '8% 2% 2% 2%'
+    }
+  },
+
+  // iPad Pro 12.9" Portrait
+  '@media only screen and (max-width:1024px) and (orientation:portrait)': {
+    main: {
+      height: '100%',
+      margin: '2%',
+      display: 'flex',
+      flexDirection: 'column'
+    },
+    dashboardContainer: {
+      boxShadow: '1px 1px 3px #8C8D91'
+    },
+    trainingContentCard: {
+      flexGrow: 1,
+      margin: '5% 0% 2% 0%',
+      padding: '8% 2% 3% 2%',
+      justifyContent: 'flex-start',
+      borderTop: '2px solid #FFA726'
+    }
+  },
+
+  // iPad-Mini Landscape
+  '@media only screen and (max-width:1024px) and (orientation:landscape)': {
+    main: {
+      margin: '2%'
+    }
+  },
+
+  // Minor Breakpoint - 900px height
+  '@media only screen and (max-width:1024px) and (min-height:900px) and (orientation:portrait)': {
+    root: {
+      fontSize: '1.5em'
+    }
+  },
+
+  // Minor Breakpoint - 920px width
+  '@media only screen and (max-width:920px) and (orientation:landscape)': {
+    main: {
+      height: '100%',
+      margin: '0% 2% 0% 2%',
+      display: 'flex',
+      flexDirection: 'column'
+    },
+    dashboardContainer: {
+      boxShadow: '1px 1px 3px #8C8D91'
+    },
+    trainingContentCard: {
+      flexGrow: 1,
+      margin: '3% 0% 2% 0%',
+      padding: '5% 2% 3% 2%',
+      justifyContent: 'flex-start',
+      borderTop: '2px solid #FFA726'
+    }
+  },
+
+  // Mobile Landscape
+  '@media only screen and (max-width:600px) and (orientation:landscape)': {
+    root: {
+      fontSize: '0.7em'
+    }
   }
 };
 
 const ViewEnum = {
   CONCEPTS: 1,
-  EXAMPLE: 2,
-  DEMONSTRATION: 3,
-  TRYIT: 4,
-  KNOWLEDGECHECK: 5
+  DEFINITIONS: 2,
+  EXAMPLE: 3,
+  DEMONSTRATION: 4,
+  TRYIT: 5,
+  KNOWLEDGECHECK: 6
 };
 
-interface Style  {
+interface Props {
+  classes: Style
+}
+
+interface Style {
   root: string,
   viewButtons: string,
   buttonsList: string,
   resultsContent: string
-}
-
-interface Props {
-  classes: Style
 }
 
 interface State {
@@ -58,10 +155,13 @@ interface State {
 }
 
 /**
- * Training for Associative & Cooperative
+ * Math Instruction  training
  * @class AssociativeCooperativeInteractionsTrainingPage
  */
 class AssociativeCooperativeInteractionsTrainingPage extends React.Component<Props, State> {
+  /**
+   * @param {Props} props 
+   */
   constructor(props: Props) {
     super(props);
   }
@@ -72,37 +172,43 @@ class AssociativeCooperativeInteractionsTrainingPage extends React.Component<Pro
 
   conceptsClick = () => {
     if (this.state.view !== ViewEnum.CONCEPTS) {
-      this.setState({ view: ViewEnum.CONCEPTS });
+      this.setState({view: ViewEnum.CONCEPTS})
     }
-  };
+  }
+
+  definitionsClick = () => {
+    if (this.state.view !== ViewEnum.DEFINITIONS) {
+      this.setState({view: ViewEnum.DEFINITIONS})
+    }
+  }
 
   exampleClick = () => {
     if (this.state.view !== ViewEnum.EXAMPLE) {
-      this.setState({ view: ViewEnum.EXAMPLE });
+      this.setState({view: ViewEnum.EXAMPLE})
     }
-  };
+  }
 
   demonstrationClick = () => {
     if (this.state.view !== ViewEnum.DEMONSTRATION) {
-      this.setState({ view: ViewEnum.DEMONSTRATION });
+      this.setState({view: ViewEnum.DEMONSTRATION})
     }
-  };
+  }
 
   tryItClick = () => {
     if (this.state.view !== ViewEnum.TRYIT) {
-      this.setState({ view: ViewEnum.TRYIT });
+      this.setState({view: ViewEnum.TRYIT})
     }
-  };
+  }
 
   knowledgeCheckClick = () => {
     if (this.state.view !== ViewEnum.KNOWLEDGECHECK) {
-      this.setState({ view: ViewEnum.KNOWLEDGECHECK });
+      this.setState({view: ViewEnum.KNOWLEDGECHECK})
     }
-  };
+  }
 
   static propTypes = {
     classes: PropTypes.object.isRequired
-  };
+  }
 
   /**
    * render function
@@ -110,159 +216,74 @@ class AssociativeCooperativeInteractionsTrainingPage extends React.Component<Pro
    */
   render() {
     const { classes } = this.props;
+    const { view } = this.state;
     return (
       <div className={classes.root}>
         <FirebaseContext.Consumer>
           {(firebase: object) => <AppBar firebase={firebase} />}
         </FirebaseContext.Consumer>
-        <main>
-          <Grid
-            container
-            spacing={0}
-            justify="center"
-            direction={"row"}
-            alignItems={"center"}
-          >
-            <Grid container item xs={3}>
-              <List className={classes.buttonsList}>
-                <ListItem style={{ display: "flex", justifyContent: "center" }}>
-                  <img src={AssocCoopIconImage} width={"100vw"} />
-                </ListItem>
-                <ListItem>
-                  <Button
-                    size="large"
-                    color={"primary"}
-                    fullWidth={true}
-                    variant={
-                      this.state.view === ViewEnum.CONCEPTS
-                        ? "contained"
-                        : "outlined"
+        <div className={classes.titleContainer}>
+        <Button variant="contained" size="medium" className={classes.backButton}
+                  onClick={() => {
+                    if (this.props.location.state !== undefined) { // came from MyTeachers
+                      this.props.history.goBack();
+                    } else {
+                      this.props.history.replace({
+                        pathname: "/Magic8Menu",
+                        state: {type: "Training"}
+                      })
                     }
-                    className={classes.viewButtons}
-                    onClick={this.conceptsClick}
-                  >
-                    CONCEPTS
-                  </Button>
-                </ListItem>
-                <ListItem>
-                  <Button
-                    size="large"
-                    color={"primary"}
-                    fullWidth={true}
-                    variant={
-                      this.state.view === ViewEnum.EXAMPLE
-                        ? "contained"
-                        : "outlined"
-                    }
-                    className={classes.viewButtons}
-                    onClick={this.exampleClick}
-                  >
-                    EXAMPLE
-                  </Button>
-                </ListItem>
-                <ListItem>
-                  <Button
-                    size="large"
-                    color={"primary"}
-                    fullWidth={true}
-                    variant={
-                      this.state.view === ViewEnum.DEMONSTRATION
-                        ? "contained"
-                        : "outlined"
-                    }
-                    className={classes.viewButtons}
-                    onClick={this.demonstrationClick}
-                  >
-                    DEMONSTRATION
-                  </Button>
-                </ListItem>
-                <ListItem>
-                  <Button
-                    size="large"
-                    color={"primary"}
-                    fullWidth={true}
-                    variant={
-                      this.state.view === ViewEnum.TRYIT
-                        ? "contained"
-                        : "outlined"
-                    }
-                    className={classes.viewButtons}
-                    onClick={this.tryItClick}
-                  >
-                    TRY IT YOURSELF
-                  </Button>
-                </ListItem>
-                <ListItem>
-                  <Button
-                    size="large"
-                    color={"primary"}
-                    fullWidth={true}
-                    variant={
-                      this.state.view === ViewEnum.KNOWLEDGECHECK
-                        ? "contained"
-                        : "outlined"
-                    }
-                    className={classes.viewButtons}
-                    onClick={this.knowledgeCheckClick}
-                  >
-                    KNOWLEDGE CHECK
-                  </Button>
-                </ListItem>
-              </List>
-            </Grid>
-            <Grid
-              container
-              item
-              xs={8}
-              justify="center"
-              direction={"row"}
-              alignItems={"center"}
-            >
-              <Typography
-                variant="h5"
-              >
-                Training: Associative & Cooperative Tool
-              </Typography>
-              <Grid item xs={12}>
-                <div>
-                  {this.state.view === ViewEnum.CONCEPTS ? (
-                    <div className={classes.resultsContent}>
-                      <TrainingVideo videoUrl={"https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/AC_Concepts.mp4?alt=media&token=6499ec3f-8f39-4334-aeea-8e34a4e8fb7e"}/>
-                    </div>
-                  ) : this.state.view === ViewEnum.EXAMPLE ? (
-                    <div className={classes.resultsContent}>
-                      <Typography variant="h4">
-                        COMING SOON...
-                      </Typography>
-                    </div>
-                  ) : this.state.view === ViewEnum.DEMONSTRATION ? (
-                    <div className={classes.resultsContent}>
-                      <Typography variant="h4">
-                        COMING SOON...
-                      </Typography>
-                    </div>
-                  ) : this.state.view === ViewEnum.TRYIT ? (
-                    <div className={classes.resultsContent}>
-                      <Typography variant="h4">
-                        COMING SOON...
-                      </Typography>
-                    </div>
-                  ) : this.state
-                      .view === ViewEnum.KNOWLEDGECHECK ? (
-                    <div className={classes.resultsContent}>
-                      <TrainingQuestionnaire section={'ac'} />
-                    </div> // replace this null with next steps content
-                  ) : null}
-                </div>
-              </Grid>
-            </Grid>
-          </Grid>
-        </main>
+                  }}>
+            <ChevronLeftRoundedIcon />
+            <b>Training Home</b>
+          </Button>
+          <h1 style={{ justifySelf: 'center' }}>Training Tool</h1>
+          <Button variant='contained' size='medium' className={classes.backButton} onClick={null} style={{ visibility:'hidden' }}>
+            <ChevronLeftRoundedIcon />
+            <b>Training Home</b>
+          </Button>
+        </div>
+        <div className={classes.main}>
+          <div className={classes.dashboardContainer}>
+            <TrainingDashboard
+              ViewEnum={ViewEnum}
+              view={view}
+              Icon={AssocCoopIconImage}
+              conceptsClick={this.conceptsClick}
+              definitionsClick={this.definitionsClick}
+              exampleClick={this.exampleClick}
+              demonstrationClick={this.demonstrationClick}
+              tryItClick={this.tryItClick}
+              knowledgeCheckClick={this.knowledgeCheckClick}
+              buttonColor={Constants.MathColor}
+            />
+          </div>
+          <div className={classes.trainingContentCard}>
+            {view === ViewEnum.CONCEPTS ? 
+              <TrainingVideo
+                videoUrl={'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/AC_Concepts.mp4?alt=media&token=6499ec3f-8f39-4334-aeea-8e34a4e8fb7e'} />
+            : view === ViewEnum.DEFINITIONS ?
+              <AssocCoopHelpCard />
+            : view === ViewEnum.EXAMPLE ? 
+              <div>EXAMPLE</div>
+            : view === ViewEnum.DEMONSTRATION ? 
+              <div>
+                <TrainingVideo
+                  videoUrl={'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/TT_Demo.mp4?alt=media&token=6fd2c698-0b5e-4a88-94d9-c34637a85043'}
+                />
+              </div>
+            : view === ViewEnum.TRYIT ? 
+              <div>TRY IT</div>
+            : view === ViewEnum.KNOWLEDGECHECK ? (
+              <TrainingQuestionnaire section={'assoccoop'}/>
+            ) : null}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default withStyles(styles)(
-  AssociativeCooperativeInteractionsTrainingPage
-);
+export default withStyles(styles)(AssociativeCooperativeInteractionsTrainingPage);
+
+
