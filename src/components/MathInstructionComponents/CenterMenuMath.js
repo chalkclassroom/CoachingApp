aac374faba05be853b4781b21cf2ab30724423b6
vurajeev <58226973+vurajeev@users.noreply.github.com>
2020-02-12 08:53:56 -0600

    functions and db

--- src/components/MathInstructionComponents/CenterMenuMath.js
+++ src/components/MathInstructionComponents/CenterMenuMath.js
@@ -6,110 +6,52 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Grid from "@material-ui/core/Grid";
-import ReplySharpIcon from '@material-ui/icons/ReplySharp';
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-//import Box from "@material-ui/core/Box";
-import styled from 'styled-components';
-import { spacing,typography, palette } from '@material-ui/system';
+import grey from "@material-ui/core/colors/grey";
 import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core/styles";
 import {
   addNewCenter,
   incrementCenterCount
 } from "../../state/actions/math-instruction";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
 import PropTypes from "prop-types";
 import Dashboard from "../Dashboard";
 import TotalVisitCount from "../TotalVisitCount";
-import { Fab } from '@material-ui/core';
-import { toggleLOISettingType } from '../../state/actions/level-of-instruction';
-
 
 // TODO: X in top right corner, press and hold to remove/edit the center.
 
-const Box = styled.div`${palette}${spacing}${typography}`;
-
-
-// const style = {
-//     '@media only screen and (max-width:768px) and (orientation:portrait)': {
-
-//   root: {
-//     position: "relative!important",
-//     left: "-10rem!important",
-//     top:" 1.5rem!important",
-//     padding: "0 216px!important",
-//   }
-//   }
-// }
-
-const styles = (theme) => ({
- /*  '@media only screen and (max-width:768px)': {
-		root: {
-      position: "relative!important",
-      left: "-10rem!important",
-      top:" 1.5rem!important",
-      padding: "0 216px!important",
-    }
-  }, */
-  '@media only screen and (max-width:834px) and (orientation:portrait)': {
-    // strip: {
-    //   position: "relative!important",
-    //   left: "-10rem!important",
-    //   top:" 1.5rem!important",
-    //   padding: "0 216px!important",
-    // }
-	},
-
+const styles = theme => ({
   root: {
-    border: 0,
-    borderRadius: 3,
-    color: "#fff !important",
-    height: 48,
-    padding: "0 80%",
-    borderRadius:"3px",
-    // marginLeft: "-95%",
-    marginTop: "28%",
-  // position: "relative",
-  // left: "-14rem",
-  // top:" 0.5rem",
-marginLeft: "-10%",
-
+    flexGrow: 1,
+    backgroundColor: "#ffffff",
+    display: "flex",
+    flexDirection: "column"
   },
-  button: {
-		margin: theme.spacing.unit,
-		width: 150,
-		height: 150,
-		textAlign: 'center',
-		display: "flex",
-		flexDirection: "column",
-   // fontFamily: "Arimo",
-    color: "#fff !important",
-   zIndex: "99",
-  
-  // position: "absolute"
-
-    	//backgroundColor: '#27B78FFF'
-	},
   grow: {
     flexGrow: 1
   }
 });
 
-
 const VisitCenterButton = ({ centerName, visitCount, onClick }) => {
   const hsl = Math.max(82 - 4 * visitCount, 54);
   return (
-    <Fab
+    <Button
+      variant="contained"
       //color="primary" //color
       style={{
-        width: 150,
-        height: 150,
+        minHeight: 150,
+        maxHeight: 150,
+        minWidth: 150,
+        maxWidth: 150,
         whiteSpace: "normal",
         wordWrap: "break-word",
-        fontFamily: "Arimo",
-        backgroundColor: '#27B78FFF',
-        color: "white"
+        backgroundColor: `hsl(14, 78%, ${hsl}%`//hsl code to red/orange-math
       }}
       onClick={onClick}
     >
@@ -117,7 +59,7 @@ const VisitCenterButton = ({ centerName, visitCount, onClick }) => {
       <br />
       <br />
       {visitCount}
-    </Fab>
+    </Button>
   );
 };
 
@@ -144,8 +86,7 @@ const commonSecondHalf = commonCenters.slice(
 
 class CenterChecklist extends React.Component {
   state = {
-    checked: [],
-    settingtype: []
+    checked: []
   };
 
   handleToggle = value => () => {
@@ -164,91 +105,93 @@ class CenterChecklist extends React.Component {
     });
   };
 
-  	/**
-   * @param {string} settingtype
-   */
-  handleButtonChange = (settingtype) => {
-		this.props.toggleLOISettingType(settingtype);
-		this.setState({
-			selected: settingtype
-		});
-	};
-
   handleDone = () => {
     this.state.checked.forEach(checked => {
-this.props.toggleLOISettingType(checked) });
+      this.props.addCenter(checked);
+    });
     this.props.switchToCenterMenu();
   };
 
-  render() 
-  {
-    const { classes } = this.props;
-
+  render() {
     return (
       <div>
-          <Grid container spacing={16} alignItems="center">
-
-               
-                     <Grid item xs={3}>
-                       <Grid
-                         container
-                         alignItems={"center"}
-                         justify={"center"}
-                         direction={"row"}
-                       >
-                         <Dashboard
-                           magic8="Math Instruction"
-                           color="#E55529"
-                           infoPlacement="flex-start"
-                           completeObservation={true}
-                         />
-                       </Grid>
-                 </Grid>
-                 <Grid item xs={9} justify="center">
-                 <Grid container direction="column" justify="center" alignItems="center">
-          <Typography component="h4" variant="h4"  style={{ padding: "10px", fontFamily: 'Arimo' }}
+        <Grid container direction="column" justify="center" alignItems="center">
+          <Typography
+            component="h4"
+            variant="h4"
+            align="center"
+            style={{ padding: "10px" }}
           >
-           What is the activity setting?
+            Which centers are open?
           </Typography>
-	</Grid>
-        <Grid container alignItems="flex-start" container direction={'row'} style={{ fontFamily: 'Arimo' }}>
-
-				 <Grid container alignItems="flex-start" item xl={6} md={6} sm={12} xs={12} style={{ fontFamily: 'Arimo' }}>
-					<Fab
-						onClick={() => this.handleDone('wholeGroup')}
-					//	classes={{ root: classes.button }}//, label: classes.label
-          style={ {backgroundColor: "#27B78FFF",
-          width: 150,
-         height: 150,
-         color: "white"}}
-					>
-						Whole Group
-					</Fab>
-
+          <Typography>
+            You will have the opportunity to add additional centers if the ones
+            in your classroom are not listed here.
+          </Typography>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="flex-start"
+          >
+            <Grid item>
+              <List>
+                {commonFirstHalf.map((value, index) => (
+                  <ListItem
+                    key={index}
+                    role={undefined}
+                    dense
+                    button
+                    disableRipple
+                    onClick={this.handleToggle(value)}
+                  >
+                    <Checkbox
+                      checked={this.state.checked.includes(value)}
+                      tabIndex={-1}
+                      disableRipple
+                    />
+                    <ListItemText
+                      primary={value}
+                      style={{ whiteSpace: "normal", wordWrap: "break-word" }}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
+            <Grid item>
+              <List>
+                {commonSecondHalf.map((value, index) => (
+                  <ListItem
+                    key={index}
+                    role={undefined}
+                    dense
+                    button
+                    disableRipple
+                    onClick={this.handleToggle(value)}
+                  >
+                    <Checkbox
+                      checked={this.state.checked.includes(value)}
+                      tabIndex={-1}
+                      disableRipple
+                    />
+                    <ListItemText
+                      primary={value}
+                      style={{ whiteSpace: "normal", wordWrap: "break-word" }}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
           </Grid>
-          <Grid container alignItems="flex-start" item xl={6} md={6} sm={12} xs={12} style={{ fontFamily: 'Arimo' }}>
-
-					<Fab
-						onClick={() => this.handleDone('centersOrSmall')}
-					//classes={{ root: classes.button }}//, label: classes.label
-            style={ {backgroundColor: "#27B78FFF",
-                     width: 150,
-                    height: 150,
-                    color: "white"}}
-					>
-						Centers/Small Group
-					</Fab>
-				</Grid>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.handleDone}
+          >
+            Done
+          </Button>
         </Grid>
-        </Grid>
-
-                         
-   
-        </Grid>
-   
-   
-                  
-             </div>
+      </div>
     );
   }
 }
@@ -261,9 +204,9 @@ class NewCenterDialog extends React.Component {
         onClose={this.props.handleClose}
         aria-labelledby="form-dialog-title"
       >
-        <DialogTitle id="form-dialog-title"  style={{fontFamily: 'Arimo'}}>Add a New Center</DialogTitle>
+        <DialogTitle id="form-dialog-title">Add a New Center</DialogTitle>
         <DialogContent>
-          <DialogContentText  style={{fontFamily: 'Arimo'}} >
+          <DialogContentText>
             Please enter the name of the new center.
           </DialogContentText>
           <TextField
@@ -277,16 +220,12 @@ class NewCenterDialog extends React.Component {
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={this.props.handleClose} 
-          color="primary"
-          style={{fontFamily: 'Arimo'}}
-          >
+          <Button onClick={this.props.handleClose} color="primary">
             Cancel
           </Button>
           <Button
             onClick={() => this.props.handleSubmit(this.centerName.value)}
             color="primary"
-            style={{fontFamily: 'Arimo'}}
           >
             Add Center
           </Button>
@@ -306,7 +245,7 @@ class CenterMenuMath extends React.Component {
     const mEntry = {
       teacher: this.props.teacherId,
       observedBy: this.props.firebase.auth.currentUser.uid,
-      type: "Math"
+      type: "AC"
     };
     this.props.firebase.handleSession(mEntry);
   }
@@ -360,8 +299,6 @@ class CenterMenuMath extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
-
     switch (this.state.status) {
       case CENTER_CHECKLIST:
         return (
@@ -389,110 +326,59 @@ class CenterMenuMath extends React.Component {
                   <Grid item xs={3}>
                     <Grid
                       container
-                      alignItems={"right"}
-                      justify={"right"}
+                      alignItems={"center"}
+                      justify={"center"}
                       direction={"column"}
                     >
                       {/* <div style={{ margin: 20 }} /> */}
                       <Dashboard
                         magic8="Math Instruction"
                         color="#E55529"
-
+                        infoDisplay={
+                          <TotalVisitCount count={this.state.totalVisitCount} />
+                        }
                         infoPlacement="flex-start"
                         completeObservation={true}
                       />
                     </Grid>
-              </Grid>
-                  <Grid container xs={9}  container direction={'row'} >
-                  <Grid container alignItems="flex-start" item xl={4} md={4} sm={4} xs={4} style={{ fontFamily: 'Arimo' }}></Grid>
-
-                   <Grid container alignItems="flex-start" item xl={1} md={2} sm={2} xs={2} style={{ fontFamily: 'Arimo' }}>
-					<Fab
-						//onClick={() => this.handleButtonChange('wholeGroup')}
-           // classes={{ root: classes.button }}//, label: classes.label
-            className={classes.button}
-
-            style={{ backgroundColor: "#38761dff" }}
-					>
-            Ask High-Level Question
-					</Fab>
-          </Grid>
-
-          <Grid container alignItems="flex-start" item md={2} style={{ fontFamily: 'Arimo' }}>
-
-         
-           <Button disabled          
-                  style={{ backgroundColor: "#6aa84fff",color: "#fff!important"}}
-                  className={classes.root}>
-                     Inferential Instruction
-          </Button> 
-         
-
-          </Grid>
-          <Grid container alignItems="flex-start" item xl={1} md={2} sm={2} xs={2} style={{ fontFamily: 'Arimo' }}>
-					<Fab
-					//	onClick={() => this.handleButtonChange('centersOrSmall')}
-          classes={{ root: classes.button }}//, label: classes.label
-          style={{ backgroundColor: "#38761dff"}}
-          >
-          Follow-up on Children’s Responses
-					</Fab>
-			 	 </Grid>
-          <Grid container alignItems="flex-start" item xl={4} md={4} sm={4} xs={4} style={{ fontFamily: 'Arimo' }}></Grid>
-
-        <Grid container alignItems="flex-start" item xl={3} md={3} sm={12} xs={12} style={{ fontFamily: 'Arimo' }}>
-					
-          </Grid>
-
-          <Grid container alignItems="flex-start" item md={2} style={{ fontFamily: 'Arimo' }}>
-          <text>
-          <ReplySharpIcon width={50} height={50} > 0</ReplySharpIcon>
-          </text>
-          </Grid>
-
-
-          <Grid container alignItems="flex-start" item xl={7} md={7} sm={12} xs={12} style={{ fontFamily: 'Arimo' }}>
-
-			
-		  		</Grid>
-
-        <Grid container alignItems="flex-start" item xl={3} md={3} sm={12} xs={12} style={{ fontFamily: 'Arimo' }}>
-					<Fab
-						//onClick={() => this.handleButtonChange('wholeGroup')}
-						classes={{ root: classes.button }}//, label: classes.label
-            style={{ backgroundColor: "#1155ccff" }}//,{ zIndex: 100 }
-					>
-            Ask Low-Level Question
-					</Fab>
-          </Grid>
-
-          <Grid container alignItems="flex-start" item md={2} style={{ fontFamily: 'Arimo' }}>
-
-         
-          {/* <Box bgcolor="#6aa84fff" color="white" fontFamily= "Arimo"  p={1}>Inferential Instruction</Box>  */}
-          <Button disabled          
-                  style={{ backgroundColor: "#6d9eebff",color: "#fff!important"}}
-                 className={classes.root}>
-Basic Skills Instruction
-                   </Button>
-          </Grid>
-
-          <Grid container alignItems="flex-start" item xl={7} md={7} sm={12} xs={12} style={{ fontFamily: 'Arimo' }}>
-
-					<Fab
-					//	onClick={() => this.handleButtonChange('centersOrSmall')}
-         classes={{ root: classes.button }}//, label: classes.label
-          style={{ backgroundColor: "#1155ccff"}}
-        >
-Teach Specific Skills					</Fab>
-				</Grid>
-
-      </Grid>
-                    
-    </Grid>
-
+                  </Grid>
+                  <Grid container xs={9}>
+                    {this.props.centers.map((center, index) => (
+                      <Grid
+                        item
+                        xs={4}
+                        style={{ textAlign: "center", padding: "10px" }}
+                      >
+                        <VisitCenterButton
+                          centerName={center.name}
+                          visitCount={center.count}
+                          onClick={() => this.handleCenterVisit(center.name)}
+                        />
+                      </Grid>
+                    ))}
+                    <Grid
+                      item
+                      xs={4}
+                      style={{ textAlign: "center", padding: "10px" }}
+                    >
+                      <Button
+                        variant="contained"
+                        style={{
+                          minHeight: 150,
+                          maxHeight: 150,
+                          minWidth: 150,
+                          maxWidth: 150,
+                          backgroundColor: grey[400]
+                        }}
+                        onClick={this.handleClickOpen}
+                      >
+                        Add Center <br /> <br /> +
+                      </Button>
+                    </Grid>
+                  </Grid>
                 </Grid>
               </Grid>
+            </Grid>
           </div>
         );
       case RATING_SCREEN:
--- src/views/protected/LevelOfInstructionViews/LOISettingTypeSel.js
+++ src/views/protected/LevelOfInstructionViews/LOISettingTypeSel.js
@@ -12,10 +12,9 @@ import styled from 'styled-components';
 import { spacing, typography, palette } from '@material-ui/system';
 import { connect } from 'react-redux';
 import { addNewCenter, incrementCenterCount } from '../../../state/actions/math-instruction';
+//import CapitalizedText from "./CapitalizedText";
 
 // TODO: X in top right corner, press and hold to remove/edit the center.
-
-const Box = styled.div`${palette}${spacing}${typography}`;
 
 // const style = {
 //     '@media only screen and (max-width:768px) and (orientation:portrait)': {
@@ -48,15 +47,21 @@ const styles = (theme) => ({
 	},
 
 	root: {
+		borderWidth: 1,
+		borderColor: '#000',
 		border: 0,
 		borderRadius: 3,
 		color: '#fff !important',
 		height: 48,
-		width:'100%',
+		width: '100%',
 		// padding: '0 80%',
 		borderRadius: '3px',
 		// marginLeft: "-95%",
 		marginTop: '35%',
+		textTransform: 'Capitalize',
+		fontWeight:'700',
+		fontSize:'30',
+		fontFamily: "Arimo",
 		// position: "relative",
 		// left: "-14rem",
 		// top:" 0.5rem",
@@ -69,9 +74,12 @@ const styles = (theme) => ({
 		textAlign: 'center',
 		display: 'flex',
 		flexDirection: 'column',
-		// fontFamily: "Arimo",
+	    fontFamily: "Arimo",
 		color: '#fff !important',
-		zIndex: '99'
+		zIndex: '99',
+		textTransform: 'Capitalize',
+		fontWeight:'700',
+		fontSize:'30'
 
 		// position: "absolute"
 
@@ -147,7 +155,12 @@ class CenterChecklist extends React.Component {
 									backgroundColor: '#27B78FFF',
 									width: 200,
 									height: 200,
-									color: 'white'
+									color: 'white',
+									textTransform: 'Capitalize',
+									fontWeight:'700',
+									fontSize:'30',
+									fontFamily: "Arimo",
+
 								}}
 							>
 								Whole Group
@@ -170,7 +183,13 @@ class CenterChecklist extends React.Component {
 									backgroundColor: '#27B78FFF',
 									width: 200,
 									height: 200,
-									color: 'white'
+									color: 'white',
+									textTransform: 'Capitalize',
+									fontWeight:'700',
+									fontSize:'30',
+									fontFamily: "Arimo",
+
+
 								}}
 							>
 								Centers/Small Group
@@ -305,11 +324,8 @@ class LOISettingTypeSel extends React.Component {
 								</Grid>
 							</Grid> */}
 
-
-
 							<Grid justify="flex-start" alignItems="center" direction="row">
 								<Grid container spacing={0} direction="row" alignItems="center">
-									
 									<Grid container xs={12} container direction={'row'}>
 										<Grid
 											container
@@ -325,11 +341,13 @@ class LOISettingTypeSel extends React.Component {
 												//onClick={() => this.handleButtonChange('wholeGroup')}
 												// classes={{ root: classes.button }}//, label: classes.label
 												className={classes.button}
-												style={{ backgroundColor: '#38761dff',
-												width: 200,
-												height: 200 }} //,{ zIndex: 100 }
+												style={{
+													backgroundColor: '#38761dff',
+													width: 200,
+													height: 200
+												}} //,{ zIndex: 100 }
 											>
-											Ask High-Level Question
+												Ask High-Level Question
 											</Fab>
 										</Grid>
 
@@ -344,8 +362,9 @@ class LOISettingTypeSel extends React.Component {
 												disabled
 												style={{ backgroundColor: '#6aa84fff', color: '#fff!important' }}
 												className={classes.root}
+												variant={outline-secondary}
 											>
-													Inferential Instruction
+												Inferential Instruction
 											</Button>
 										</Grid>
 										<Grid
@@ -361,9 +380,11 @@ class LOISettingTypeSel extends React.Component {
 											<Fab
 												//	onClick={() => this.handleButtonChange('centersOrSmall')}
 												classes={{ root: classes.button }} //, label: classes.label
-												style={{ backgroundColor: '#38761dff',
-												width: 200,
-												height: 200 }}
+												style={{
+													backgroundColor: '#38761dff',
+													width: 200,
+													height: 200
+												}}
 											>
 												Follow-up on Children’s Responses
 											</Fab>
@@ -372,83 +393,84 @@ class LOISettingTypeSel extends React.Component {
 								</Grid>
 							</Grid>
 
+							<Grid container xs={12} container direction={'row'}>
+								<Grid
+									container
+									alignItems="flex-start"
+									item
+									xl={4}
+									md={4}
+									sm={4}
+									xs={12}
+									style={{ fontFamily: 'Arimo' }}
+								/>
+
+								<Grid
+									container
+									alignItems="center"
+									justify="center"
+									item
+									md={4}
+									style={{ fontFamily: 'Arimo' }}
+									alignItems="center"
+								>
+									<div width={100} height={100} style={{ fontSize: '80px' }}>
+										0
+									</div>
+								</Grid>
+
+								<Grid
+									container
+									alignItems="flex-start"
+									item
+									xl={4}
+									md={4}
+									sm={4}
+									xs={12}
+									style={{ fontFamily: 'Arimo' }}
+								/>
+							</Grid>
 
 							<Grid container xs={12} container direction={'row'}>
-							<Grid
-								container
-								alignItems="flex-start"
-								item
-								xl={4}
-								md={4}
-								sm={4}
-								xs={12}
-								style={{ fontFamily: 'Arimo' }}
-							/>
+								<Grid
+									container
+									alignItems="flex-start"
+									item
+									xl={4}
+									md={4}
+									sm={4}
+									xs={12}
+									style={{ fontFamily: 'Arimo' }}
+								/>
 
-							<Grid
-								container
-								alignItems="center"
-								justify="center"
-								item
-								md={4}
-								style={{ fontFamily: 'Arimo' }}
-								alignItems="center"
-							>
-								<div  width={100} height={100} style={{fontSize:'80px'}}>
-									0</div>
-							</Grid>
+								<Grid
+									container
+									alignItems="center"
+									justify="center"
+									item
+									md={4}
+									style={{ fontFamily: 'Arimo' }}
+									alignItems="center"
+								>
+									<Button>
+										<ReplySharpIcon style={{ fontSize: '80px' }} width={100} height={100} />
+									</Button>
+								</Grid>
 
-							<Grid
-								container
-								alignItems="flex-start"
-								item
-								xl={4}
-								md={4}
-								sm={4}
-								xs={12}
-								style={{ fontFamily: 'Arimo' }}
-							/>
-							</Grid>
-
-							<Grid container xs={12} container direction={'row'}>
-							<Grid
-								container
-								alignItems="flex-start"
-								item
-								xl={4}
-								md={4}
-								sm={4}
-								xs={12}
-								style={{ fontFamily: 'Arimo' }}
-							/>
-
-							<Grid
-								container
-								alignItems="center"
-								justify="center"
-								item
-								md={4}
-								style={{ fontFamily: 'Arimo' }}
-								alignItems="center"
-							>
-								<ReplySharpIcon  style={{fontSize:'80px'}} width={100} height={100} />
-							</Grid>
-
-							<Grid
-								container
-								alignItems="flex-start"
-								item
-								xl={4}
-								md={4}
-								sm={4}
-								xs={12}
-								style={{ fontFamily: 'Arimo' }}
-							/>
+								<Grid
+									container
+									alignItems="flex-start"
+									item
+									xl={4}
+									md={4}
+									sm={4}
+									xs={12}
+									style={{ fontFamily: 'Arimo' }}
+								/>
 							</Grid>
 
 							<Grid justify="flex-start" alignItems="center" direction="row">
 								<Grid container spacing={0} direction="row" alignItems="center">
-									
 									<Grid container xs={12} container direction={'row'}>
 										<Grid
 											container
@@ -464,9 +486,11 @@ class LOISettingTypeSel extends React.Component {
 												//onClick={() => this.handleButtonChange('wholeGroup')}
 												// classes={{ root: classes.button }}//, label: classes.label
 												className={classes.button}
-												style={{ backgroundColor: '#1155ccff',
-												width: 200,
-												height: 200 }} //,{ zIndex: 100 }
+												style={{
+													backgroundColor: '#1155ccff',
+													width: 200,
+													height: 200
+												}} //,{ zIndex: 100 }
 											>
 												Ask Low-Level Question
 											</Fab>
@@ -500,9 +524,11 @@ class LOISettingTypeSel extends React.Component {
 											<Fab
 												//	onClick={() => this.handleButtonChange('centersOrSmall')}
 												classes={{ root: classes.button }} //, label: classes.label
-												style={{ backgroundColor: '#1155ccff',
-												width: 200,
-												height: 200 }}
+												style={{
+													backgroundColor: '#1155ccff',
+													width: 200,
+													height: 200
+												}}
 											>
 												Teach Specific Skills
 											</Fab>
