dd653545e00478a7f8314749a97ee5e9b59e4589
clarah2 <clare.speer@gmail.com>
2020-06-04 12:44:12 -0500

    adding close button to look fors modal and adding to results page

--- src/components/StudentEngagementComponents/StudentEngagementHelp.tsx
+++ src/components/StudentEngagementComponents/StudentEngagementHelp.tsx
@@ -4,87 +4,71 @@ import { withStyles } from "@material-ui/core/styles";
 import StudentEngagementHelpCard from './StudentEngagementHelpCard';
 import Grid from "@material-ui/core/Grid";
 import Modal from "@material-ui/core/Modal";
+import CloseIcon from "@material-ui/icons/Close";
+import Tooltip from "@material-ui/core/es/Tooltip/Tooltip";
+import IconButton from "@material-ui/core/es/IconButton/IconButton";
 
 /**
  * specifies styling for modal
  * @return {css}
  */
 function getModalStyle(): React.CSSProperties {
-    return {
-        position: "fixed",
-        top: `50%`,
-        left: `50%`,
-        transform: `translate(-50%, -50%)`
-    } as React.CSSProperties;
+  return {
+    position: "fixed",
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`
+  } as React.CSSProperties;
 }
 
 const styles: object = {
-    paper: {
-        position: "absolute",
-        width: "67%",
-        backgroundColor: 'white',
-        padding: '2em',
-        borderRadius: 8,
-    }
+  paper: {
+    position: "absolute",
+    width: "67%",
+    backgroundColor: 'white',
+    padding: '2em',
+    borderRadius: 8,
+  }
 };
 
 interface Props {
-    classes: Style
-}
-
-interface Style {
+  classes: {
     paper: string,
+  },
+  open: boolean,
+  close(): void
 }
 
-interface State {
-    open: boolean
-}
 
 /**
- * sequential activities look-fors
- * @class SequentialActivitiesHelp
+ * student engagement look-fors
+ * @param {Props} props
+ * @return {ReactElement}
+ * @class StudentEngagementHelp
  */
-class StudentEngagementHelp extends React.Component<Props, State> {
-    state = {
-        open: true
-    };
-
-    handleOpen = (): void => {
-        this.setState({ open: true });
-    };
-
-    handleClose = (): void => {
-        this.setState({ open: false });
-    };
-
-    static propTypes = {
-        classes: PropTypes.object.isRequired
-    }
-
-    /**
-     * render function
-     * @return {ReactNode}
-     */
-    render(): React.ReactNode {
-        const { classes } = this.props;
-
-        return (
-            <div>
-                <Modal open={this.state.open}>
-                    <div style={getModalStyle()} className={classes.paper}>
-                        <Grid
-                            container
-                            alignItems="center"
-                            direction="column"
-                            justify="flex-start"
-                        >
-                            <StudentEngagementHelpCard />
-                        </Grid>
-                    </div>
-                </Modal>
-            </div>
-        );
-    }
+function StudentEngagementHelp(props: Props): React.ReactElement {
+  const { classes, open, close } = props;
+  return (
+    <div>
+      <Modal open={open}>
+        <div style={getModalStyle()} className={classes.paper}>
+          <Grid container direction="row">
+            <Grid item xs={11} />
+            <Grid item xs={1}>
+              <IconButton style={{ padding: 10 }}>
+                <Tooltip title={"Close"} placement={"right"}>
+                  <CloseIcon
+                    onClick={close}
+                  />
+                </Tooltip>
+              </IconButton>
+            </Grid>
+          </Grid>
+          <StudentEngagementHelpCard />
+        </div>
+      </Modal>
+    </div>
+  );
 }
 
 export default withStyles(styles)(StudentEngagementHelp);
