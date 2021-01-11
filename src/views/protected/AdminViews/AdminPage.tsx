c81e783c3fa8d3847a457dd4ae0d8cfa6dc49f14
Chris Thompson <chris@233analytics.com>
2021-01-11 13:09:30 -0600

    updates to admin page

--- src/views/protected/AdminViews/AdminPage.tsx
+++ src/views/protected/AdminViews/AdminPage.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
-import { Button, FormControl, Input, InputLabel, MenuItem, Select } from '@material-ui/core'
+import { Button, FormControl, Input, InputLabel, MenuItem, Select, makeStyles } from '@material-ui/core'
 import * as xlsx from 'xlsx'
 import CHALKLogoGIF from '../../../assets/images/CHALKLogoGIF.gif'
+import { FirebaseContext } from '../../../components/Firebase'
+import * as Types from '../../../constants/Types'
 
 interface Props {
     isAdmin: boolean
@@ -23,107 +25,112 @@ const MenuProps = {
     },
 }
 
+const useStyles = makeStyles(_ => ({
+    root: {
+        flexGrow: 1,
+        height: "100vh",
+        flexDirection: "column",
+        overflowX: 'hidden',
+        overflowY: 'auto'
+    },
+    formControl: {
+      minWidth: 120
+    },
+    comparisonText: {
+        paddingLeft: '1em',
+        lineHeight: '0.8em',
+        fontFamily: 'Arimo'
+    }
+}));
+
+
+/**
+ *
+ * @param {Event} event
+ */
+const handleChange = (event: React.ChangeEvent<{ value: unknown }>, setSelectedTables: Function) => {
+    const { value } = event.target
+    setSelectedTables(value as string[])
+};
 
 /**
  *
  */
-class AdminPage extends React.Component<Props, State> {
-
-    /**
-     *
-     * @param {Props} props
-     */
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            loading: false,
-            selectedTables: [],
+const generateExcel = async (selectedTables: Array<string>, setLoading: Function, firebase: object) => {
+    setLoading(true)
+    const results = await Promise.all(selectedTables.map(async t => {
+        return {
+            table: t,
+            data: await firebase.fetchExport(t),
         }
-    }
+    }))
 
-    /**
-     *
-     */
-    async generateExcel() {
-        this.setState({ loading: true })
-        const results = await Promise.all(this.state.selectedTables.map(async t => {
-            return {
-                table: t,
-                data: await this.context.fetchExport(t),
-            }
-        }))
+    const wb = xlsx.utils.book_new()
+    results.forEach(r => {
+        const wsName = r.table
+        const wsData = r.data.split('\n').map(d => d.split(','))
+        const ws = xlsx.utils.aoa_to_sheet(wsData)
+        xlsx.utils.book_append_sheet(wb, ws, wsName)
+    })
 
-        const wb = xlsx.utils.book_new()
-        results.forEach(r => {
-            const wsName = r.table
-            const wsData = r.data.split('\n').map(d => d.split(','))
-            const ws = xlsx.utils.aoa_to_sheet(wsData)
-            xlsx.utils.book_append_sheet(wb, ws, wsName)
-        })
-
-        xlsx.writeFile(wb, 'CoachingAppExport.xlsx')
-        this.setState({ loading: true })
-    }
-
-    /**
-     *
-     * @param {Event} event
-     */
-    handleChange(event: React.ChangeEvent<{ value: unknown }>) {
-        const { value } = event.target
-        this.setState({ selectedTables: value as string[] })
-    };
-
-    /**
-     * @return {ReactNode}
-     */
-    render(): React.ReactNode {
-
-        return <div>
-            {this.state.loading ? <img src={CHALKLogoGIF} alt="Loading" width="80%" /> :
-                <React.Fragment>
-                    <FormControl>
-                        <InputLabel id="demo-mutiple-name-label">Name</InputLabel>
-                        <Select
-                            labelId="demo-mutiple-name-label"
-                            id="demo-mutiple-name"
-                            multiple
-                            value={this.state.selectedTables}
-                            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => this.handleChange(event)}
-                            input={<Input />}
-                            MenuProps={MenuProps}
-                        >
-                            <MenuItem value="ac">
-                                Associative Cooperative
-                            </MenuItem>
-                            <MenuItem value="climate">
-                                Classroom Climate
-                            </MenuItem>
-                            <MenuItem value="engagement">
-                                Engagement
-                            </MenuItem>
-                            <MenuItem value="level">
-                                Level
-                            </MenuItem>
-                            <MenuItem value="listening">
-                                Listening
-                            </MenuItem>
-                            <MenuItem value="math">
-                                Math
-                            </MenuItem>
-                            <MenuItem value="Sequential">
-                                Sequential
-                            </MenuItem>
-                            <MenuItem value="Transition">
-                                Transition
-                            </MenuItem>
-                        </Select>
-                    </FormControl>
-                    <Button onClick={(_) => this.generateExcel()}>Export</Button>
-                </React.Fragment>
-            }
-        </div>
-    }
+    xlsx.writeFile(wb, 'CoachingAppExport.xlsx')
+    setLoading(false)
+}
+/**
+ * @return {ReactNode}
+ */
+const AdminPage = ():React.ReactNode => {
+    const classes = useStyles();
+    const [loading, setLoading] = React.useState(false);
+    const [selectedTables, setSelectedTables] = React.useState([])
+    return <div className={classes.root}>
+        {loading ? <img src={CHALKLogoGIF} alt="Loading" width="80%" /> :
+            <React.Fragment>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="demo-mutiple-name-label">Name</InputLabel>
+                    <Select
+                        labelId="demo-mutiple-name-label"
+                        id="demo-mutiple-name"
+                        multiple
+                        value={selectedTables}
+                        onChange={(event: React.ChangeEvent<HTMLSelectElement>) => handleChange(event, setSelectedTables)}
+                        input={<Input />}
+                        MenuProps={MenuProps}
+                    >
+                        <MenuItem value="ac">
+                            Associative Cooperative
+                        </MenuItem>
+                        <MenuItem value="climate">
+                            Classroom Climate
+                        </MenuItem>
+                        <MenuItem value="engagement">
+                            Engagement
+                        </MenuItem>
+                        <MenuItem value="level">
+                            Level
+                        </MenuItem>
+                        <MenuItem value="listening">
+                            Listening
+                        </MenuItem>
+                        <MenuItem value="math">
+                            Math
+                        </MenuItem>
+                        <MenuItem value="Sequential">
+                            Sequential
+                        </MenuItem>
+                        <MenuItem value="Transition">
+                            Transition
+                        </MenuItem>
+                    </Select>
+                </FormControl>
+                <FirebaseContext.Consumer>
+                    {firebase => (
+                        <Button
+                            onClick={(_) => generateExcel(selectedTables, setLoading, firebase)}>Export</Button>)}
+                </FirebaseContext.Consumer>
+            </React.Fragment>
+        }
+    </div>
 }
 
 export default AdminPage
