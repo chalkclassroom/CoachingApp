21201c9e6df6c096e8275a48f8d3ddd0e36b8e3f
clarah2 <clare.speer@gmail.com>
2021-06-15 13:49:40 -0500

    updated funcRecentObservations

--- functions/funcRecentObservations/index.js
+++ functions/funcRecentObservations/index.js
@@ -16,17 +16,38 @@ exports.funcRecentObservations = functions.https.onCall(async(data, context) => 
   //SQL query to get number of checks for each item on checklist
   const sqlQuery = `SELECT ac.teacher AS teacherAC, ac.sessionStart AS sessionStartAC, ac.id AS idAC,
           sequential.sessionStart AS sessionStartSA, sequential.id AS idSA, sequential.teacher AS teacherSA,
-          climate.teacher as teacherCC, climate.sessionStart AS sessionStartCC, climate.id AS idCC
+          climate.teacher AS teacherCC, climate.sessionStart AS sessionStartCC, climate.id AS idCC,
+          transition.teacher AS teacherTT, transition.sessionStart AS sessionStartTT, transition.id AS idTT,
+          listening.teacher AS teacherLC, listening.sessionStart AS sessionStartLC, listening.id AS idLC,
+          math.teacher AS teacherMI, math.sessionStart AS sessionStartMI, math.id AS idMI,
+          instruction.teacher AS teacherIN, instruction.sessionStart AS sessionStartIN, instruction.id AS idIN,
+          engagement.teacher AS teacherSE, engagement.sessionStart AS sessionStartSE, engagement.id AS idSE
         FROM (cqrefpwa.observations.sequential AS sequential FULL OUTER JOIN cqrefpwa.observations.ac
           AS ac ON ac.id = sequential.id AND ac.teacher = sequential.teacher)
           FULL OUTER JOIN cqrefpwa.observations.climate AS climate ON ac.id = climate.id AND ac.teacher = climate.teacher
+          FULL OUTER JOIN cqrefpwa.observations.transition AS transition ON transition.id = climate.id
+          FULL OUTER JOIN cqrefpwa.observations.listening AS listening ON listening.id = transition.id
+          FULL OUTER JOIN cqrefpwa.observations.math AS math ON math.id = listening.id
+          FULL OUTER JOIN cqrefpwa.observations.level AS instruction ON instruction.id = math.id
+          FULL OUTER JOIN cqrefpwa.observations.engagement AS engagement ON engagement.id = instruction.id
         WHERE (ac.observedBy = '/user/`+context.auth.uid+`' AND ac.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
           OR (sequential.observedBy = '/user/`+context.auth.uid+`' AND sequential.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
           OR (climate.observedBy = '/user/`+context.auth.uid+`' AND climate.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
-        GROUP BY ac.id, ac.teacher, ac.sessionStart,
-          sequential.id, sequential.teacher, sequential.sessionStart,
-          climate.id, climate.teacher, climate.sessionStart
-        ORDER BY sessionStartAC, sessionStartSA, sessionStartCC`;
+          OR (transition.observedBy = '/user/`+context.auth.uid+`' AND transition.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+          OR (listening.observedBy = '/user/`+context.auth.uid+`' AND listening.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+          OR (math.observedBy = '/user/`+context.auth.uid+`' AND math.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+          OR (instruction.observedBy = '/user/`+context.auth.uid+`' AND instruction.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+          OR (engagement.observedBy = '/user/`+context.auth.uid+`' AND engagement.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+        GROUP BY
+          idAC, teacherAC, sessionStartAC,
+          idSA, teacherSA, sessionStartSA,
+          idCC, teacherCC, sessionStartCC,
+          idTT, teacherTT, sessionStartTT,
+          idLC, teacherLC, sessionStartLC,
+          idMI, teacherMI, sessionStartMI,
+          idIN, teacherIN, sessionStartIN,
+          idSE, teacherSE, sessionStartSE
+        ORDER BY sessionStartAC, sessionStartSA, sessionStartCC, sessionStartTT, sessionStartLC, sessionStartMI, sessionStartIN, sessionStartSE`;
 
   console.log(sqlQuery);
 
