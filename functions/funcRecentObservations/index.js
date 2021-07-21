19856a09d7aef0089aa0a3c5a2d0a9d09088bcd8
clarah2 <clare.speer@gmail.com>
2021-07-21 10:49:09 -0500

    adds literacy to recent observations cloud function

--- functions/funcRecentObservations/index.js
+++ functions/funcRecentObservations/index.js
@@ -14,52 +14,6 @@ const bigquery = new BigQuery();
 
 exports.funcRecentObservations = functions.https.onCall(async(data, context) => {
   //SQL query to get number of checks for each item on checklist
-  /* const sqlQuery = `SELECT ac.teacher AS teacherAC, ac.sessionStart AS sessionStartAC, ac.id AS idAC,
-          sequential.sessionStart AS sessionStartSA, sequential.id AS idSA, sequential.teacher AS teacherSA,
-          climate.teacher AS teacherCC, climate.sessionStart AS sessionStartCC, climate.id AS idCC,
-          transition.teacher AS teacherTT, transition.sessionStart AS sessionStartTT, transition.id AS idTT,
-          listening.teacher AS teacherLC, listening.sessionStart AS sessionStartLC, listening.id AS idLC,
-          math.teacher AS teacherMI, math.sessionStart AS sessionStartMI, math.id AS idMI,
-          instruction.teacher AS teacherIN, instruction.sessionStart AS sessionStartIN, instruction.id AS idIN,
-          engagement.teacher AS teacherSE, engagement.sessionStart AS sessionStartSE, engagement.id AS idSE,
-          CASE
-            WHEN transition.id IS NOT NULL THEN 'TT'
-            WHEN climate.id IS NOT NULL THEN 'CC'
-            WHEN math.id IS NOT NULL THEN 'MI'
-            WHEN instruction.id IS NOT NULL THEN 'IN'
-            WHEN engagement.id IS NOT NULL THEN 'SE'
-            WHEN listening.id IS NOT NULL THEN 'LC'
-            WHEN sequential.id IS NOT NULL THEN 'SA'
-            WHEN ac.id IS NOT NULL THEN 'AC'
-              ELSE 'LI'
-            END as type
-        FROM (cqrefpwa.observations.sequential AS sequential FULL OUTER JOIN cqrefpwa.observations.ac
-          AS ac ON ac.id = sequential.id AND ac.teacher = sequential.teacher)
-          FULL OUTER JOIN cqrefpwa.observations.climate AS climate ON ac.id = climate.id AND ac.teacher = climate.teacher
-          FULL OUTER JOIN cqrefpwa.observations.transition AS transition ON transition.id = climate.id
-          FULL OUTER JOIN cqrefpwa.observations.listening AS listening ON listening.id = transition.id
-          FULL OUTER JOIN cqrefpwa.observations.math AS math ON math.id = listening.id
-          FULL OUTER JOIN cqrefpwa.observations.level AS instruction ON instruction.id = math.id
-          FULL OUTER JOIN cqrefpwa.observations.engagement AS engagement ON engagement.id = instruction.id
-        WHERE (ac.observedBy = '/user/`+context.auth.uid+`' AND ac.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
-          OR (sequential.observedBy = '/user/`+context.auth.uid+`' AND sequential.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
-          OR (climate.observedBy = '/user/`+context.auth.uid+`' AND climate.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
-          OR (transition.observedBy = '/user/`+context.auth.uid+`' AND transition.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
-          OR (listening.observedBy = '/user/`+context.auth.uid+`' AND listening.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
-          OR (math.observedBy = '/user/`+context.auth.uid+`' AND math.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
-          OR (instruction.observedBy = '/user/`+context.auth.uid+`' AND instruction.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
-          OR (engagement.observedBy = '/user/`+context.auth.uid+`' AND engagement.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
-        GROUP BY
-          idAC, teacherAC, sessionStartAC,
-          idSA, teacherSA, sessionStartSA,
-          idCC, teacherCC, sessionStartCC,
-          idTT, teacherTT, sessionStartTT,
-          idLC, teacherLC, sessionStartLC,
-          idMI, teacherMI, sessionStartMI,
-          idIN, teacherIN, sessionStartIN,
-          idSE, teacherSE, sessionStartSE
-        ORDER BY sessionStartAC, sessionStartSA, sessionStartCC, sessionStartTT, sessionStartLC, sessionStartMI, sessionStartIN, sessionStartSE`; */
-
   const sqlQuery = `SELECT id, teacher, sessionStart, sessionEnd, 'AC' as type from cqrefpwa.observations.ac AS ac
     WHERE (ac.observedBy = '/user/4CPhcZa4VhOelWHk56xmN2BTwJO2' AND ac.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
     UNION DISTINCT SELECT id, teacher, sessionStart, sessionEnd, 'SA' as type FROM cqrefpwa.observations.sequential AS sequential
@@ -76,6 +30,18 @@ exports.funcRecentObservations = functions.https.onCall(async(data, context) => 
     WHERE (listening.observedBy = '/user/4CPhcZa4VhOelWHk56xmN2BTwJO2' AND listening.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
     UNION DISTINCT SELECT id, teacher, sessionStart, sessionEnd, 'SE' as type FROM cqrefpwa.observations.engagement AS engagement
     WHERE (engagement.observedBy = '/user/4CPhcZa4VhOelWHk56xmN2BTwJO2' AND engagement.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+    UNION DISTINCT SELECT id, teacher, sessionStart, sessionEnd, 'LI' as type FROM cqrefpwa.observations.literacyFoundationalChild AS literacyFoundationalChild
+    WHERE (literacyFoundationalChild.observedBy = '/user/4CPhcZa4VhOelWHk56xmN2BTwJO2' AND literacyFoundationalChild.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+    UNION DISTINCT SELECT id, teacher, sessionStart, sessionEnd, 'LI' as type FROM cqrefpwa.observations.literacyFoundationalTeacher AS literacyFoundationalTeacher
+    WHERE (literacyFoundationalTeacher.observedBy = '/user/4CPhcZa4VhOelWHk56xmN2BTwJO2' AND literacyFoundationalTeacher.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+    UNION DISTINCT SELECT id, teacher, sessionStart, sessionEnd, 'LI' as type FROM cqrefpwa.observations.literacyWritingChild AS literacyWritingChild
+    WHERE (literacyWritingChild.observedBy = '/user/4CPhcZa4VhOelWHk56xmN2BTwJO2' AND literacyWritingChild.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+    UNION DISTINCT SELECT id, teacher, sessionStart, sessionEnd, 'LI' as type FROM cqrefpwa.observations.literacyWritingTeacher AS literacyWritingTeacher
+    WHERE (literacyWritingTeacher.observedBy = '/user/4CPhcZa4VhOelWHk56xmN2BTwJO2' AND literacyWritingTeacher.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+    UNION DISTINCT SELECT id, teacher, sessionStart, sessionEnd, 'LI' as type FROM cqrefpwa.observations.literacyReadingTeacher AS literacyReadingTeacher
+    WHERE (literacyReadingTeacher.observedBy = '/user/4CPhcZa4VhOelWHk56xmN2BTwJO2' AND literacyReadingTeacher.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
+    UNION DISTINCT SELECT id, teacher, sessionStart, sessionEnd, 'LI' as type FROM cqrefpwa.observations.literacyLanguageTeacher AS literacyLanguageTeacher
+    WHERE (literacyLanguageTeacher.observedBy = '/user/4CPhcZa4VhOelWHk56xmN2BTwJO2' AND literacyLanguageTeacher.teacher != '/user/rJxNhJmzjRZP7xg29Ko6')
     ORDER BY sessionStart desc`;
 
   console.log(sqlQuery);
