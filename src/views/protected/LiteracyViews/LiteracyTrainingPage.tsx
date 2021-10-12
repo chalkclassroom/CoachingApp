b897e7e57cd77de6db5cc4e7ef62c1561efb3d46
Chris Thompson <chris@233analytics.com>
2021-10-12 12:38:33 -0400

    fixed link to book video and reformatted the file per prettier

--- src/views/protected/LiteracyViews/LiteracyTrainingPage.tsx
+++ src/views/protected/LiteracyViews/LiteracyTrainingPage.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import TrainingLayout from '../../../components/TrainingComponents/TrainingLayout';
-import LiteracyIconImage from '../../../assets/images/LiteracyIconImage.svg';
-import LiteracyInstructionHelpCard from '../../../components/LiteracyComponents/LiteracyInstructionHelpCard';
-import * as Constants from '../../../constants/Constants';
+import * as React from 'react'
+import TrainingLayout from '../../../components/TrainingComponents/TrainingLayout'
+import LiteracyIconImage from '../../../assets/images/LiteracyIconImage.svg'
+import LiteracyInstructionHelpCard from '../../../components/LiteracyComponents/LiteracyInstructionHelpCard'
+import * as Constants from '../../../constants/Constants'
 
 interface Props {
-  literacyType: number,
-  location: {
-    state: {
-      type: Constants.LiteracyTypes
+    literacyType: number
+    location: {
+        state: {
+            type: Constants.LiteracyTypes
+        }
     }
-  },
 }
 
 /**
@@ -18,30 +18,32 @@ interface Props {
  * @return {ReactElement}
  */
 export default function LiteracyTrainingPage(props: Props): React.ReactElement {
-  const { location } = props;
-  return (
-    <TrainingLayout
-      icon={LiteracyIconImage}
-      colorTheme={Constants.LiteracyTheme}
-      literacyType={location.state.type}
-      conceptsUrl={
-        location.state.type === Constants.LiteracyTypes.FOUNDATIONAL ? (
-          'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/Concepts%20LI%20FS(CC).mp4?alt=media&token=86c30b5b-130e-4b4f-a11a-618010c488c2'
-        ) : location.state.type === Constants.LiteracyTypes.WRITING ? (
-          'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/Concepts%20LI%20Writing%20(CC).mp4?alt=media&token=73315d85-4a79-4783-8e68-bd64b4b11fd4'
-        ) : location.state.type === Constants.LiteracyTypes.LANGUAGE ? (
-          'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/Concepts%20LI%20Lang%20(CC).mp4?alt=media&token=b9722bf1-9def-4f68-8f34-bc539d090658'
-        ) : location.state.type === Constants.LiteracyTypes.READING ? (
-            'https://drive.google.com/file/d/1xNAv_qin7g4Bq9mT_KqdUK4U4_HWvGf7/view?usp=sharing'
-        )
-            : ('')
-      }
-      demonstrationUrl={
-        location.state.type === Constants.LiteracyTypes.FOUNDATIONAL ? (
-          'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/Demo%20LI%20FS(CC).mp4?alt=media&token=9786bee0-fe69-471a-bb23-7181d620907d'
-        ) : ('')}
-      definitions={<LiteracyInstructionHelpCard type={location.state.type} />}
-      section='literacy'
-    />
-  );
+    const { location } = props
+    return (
+        <TrainingLayout
+            icon={LiteracyIconImage}
+            colorTheme={Constants.LiteracyTheme}
+            literacyType={location.state.type}
+            conceptsUrl={
+                location.state.type === Constants.LiteracyTypes.FOUNDATIONAL
+                    ? 'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/Concepts%20LI%20FS(CC).mp4?alt=media&token=86c30b5b-130e-4b4f-a11a-618010c488c2'
+                    : location.state.type === Constants.LiteracyTypes.WRITING
+                    ? 'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/Concepts%20LI%20Writing%20(CC).mp4?alt=media&token=73315d85-4a79-4783-8e68-bd64b4b11fd4'
+                    : location.state.type === Constants.LiteracyTypes.LANGUAGE
+                    ? 'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/Concepts%20LI%20Lang%20(CC).mp4?alt=media&token=b9722bf1-9def-4f68-8f34-bc539d090658'
+                    : location.state.type === Constants.LiteracyTypes.READING
+                    ? 'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/Book%20Reading%20Concepts_UPDATED.mp4?alt=media&token=32bf8ecd-0dc5-4080-9c76-c7385627bb72'
+                    : ''
+            }
+            demonstrationUrl={
+                location.state.type === Constants.LiteracyTypes.FOUNDATIONAL
+                    ? 'https://firebasestorage.googleapis.com/v0/b/cqrefpwa.appspot.com/o/Demo%20LI%20FS(CC).mp4?alt=media&token=9786bee0-fe69-471a-bb23-7181d620907d'
+                    : ''
+            }
+            definitions={
+                <LiteracyInstructionHelpCard type={location.state.type} />
+            }
+            section="literacy"
+        />
+    )
 }
