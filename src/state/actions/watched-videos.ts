b1787771cc6cb24c99f6a0f7d4c3c03c5665795a
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2021-10-28 17:35:33 +0200

    Clean up watched videos state & reducer

--- src/state/actions/watched-videos.ts
+++ src/state/actions/watched-videos.ts
@@ -1,11 +1,12 @@
 export const ADD_WATCHED_VIDEOS = 'add_watched_videos'
 
-export type WatchedVideosAction =
-    | { type: typeof ADD_WATCHED_VIDEOS, url: string }
-
-export const addWatchedVideos = (url: string): WatchedVideosAction => {
-    return {
-        type: ADD_WATCHED_VIDEOS,
-        url,
-    }
+export type WatchedVideosAction = {
+  type: typeof ADD_WATCHED_VIDEOS
+  url: string
 }
+
+export const addWatchedVideos = (url: string): WatchedVideosAction => ({
+  type: ADD_WATCHED_VIDEOS,
+  url,
+
+})
