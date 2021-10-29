8351a2e1a418246a59452b7e96614efbb7bdee7f
Dominik Bisiakowski <dominik.bisiakowski@makimo.pl>
2021-10-29 11:30:32 +0200

    Add modal for removing student

--- src/components/StudentEngagementComponents/CenterMenuStudentEngagement.tsx
+++ src/components/StudentEngagementComponents/CenterMenuStudentEngagement.tsx
@@ -23,6 +23,7 @@ import { connect, ConnectedProps } from 'react-redux'
 
 import ActivitySettingButtons from './ActivitySettingButtons'
 import SelectActivityModal from './SelectActivityModal'
+import RemoveStudent from './RemoveStudent.tsx'
 import StudentRatingModal from './StudentRatingModal'
 import { updateEngagementCount } from '../../state/actions/student-engagement'
 import {
@@ -78,7 +79,7 @@ interface Props extends PropsFromRedux {
     handleSession(entry: object): void
     handlePushSEEachEntry(mEntry: object): void
   }
-  handleTimerReset(): void
+  handleTimerReset(): voidF
   handleTimerStart(): void
   incrementVisitCount(): void
   observationStarted: boolean
@@ -98,6 +99,8 @@ interface State {
   setOpen: boolean
   status: Status
   studentTextFieldValue: string
+  showRemoveStudenModal: boolean
+  removeStudent: object
 }
 
 enum Status {
@@ -147,6 +150,8 @@ class CenterMenuStudentEngagement extends React.Component<Props, State> {
     entryType: -1,
     entries: 0,
     modal: false,
+    showRemoveStudenModal: false,
+    removeStudent: {},
   }
 
   studentNameInputFieldRef = React.createRef<HTMLInputElement>()
@@ -180,6 +185,20 @@ class CenterMenuStudentEngagement extends React.Component<Props, State> {
   switchToObservationPage = (): void => {
     this.setState({ status: Status.OBSERVATION })
     this.props.onStatusChange(true)
+  }
+
+  closeRemoveStudentModal = ():void => {
+    this.setState({ 
+      showRemoveStudenModal: false,
+      removeStudent: {},
+      })
+  }
+
+  openRemoveStudentModal = (removeStudent: object):void => {
+    this.setState({ 
+      removeStudent,
+      showRemoveStudenModal: true,
+       })
   }
 
   handleConfirmRating = (): void => {
@@ -485,9 +504,9 @@ class CenterMenuStudentEngagement extends React.Component<Props, State> {
                           <IconButton
                             style={{ padding: '0' }}
                             onClick={(event: SyntheticEvent): void => {
+                              this.openRemoveStudentModal(student)
                               event.preventDefault()
                               event.stopPropagation()
-                              this.removeStudent(student)
                             }}
                           >
                             <CloseIcon />
@@ -500,6 +519,12 @@ class CenterMenuStudentEngagement extends React.Component<Props, State> {
               </CardActionArea>
             </Card>
           ))}
+           {(this.state.showRemoveStudenModal ) ? 
+                <RemoveStudent 
+                  removeStudent={this.removeStudent}
+                  student={this.state.removeStudent}
+                  closeModal={this.closeRemoveStudentModal}
+                  /> : null}
           {this.state.status === Status.OBSERVATION && (
             <Grid
               container
