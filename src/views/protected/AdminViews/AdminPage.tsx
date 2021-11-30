3ec27a32232077dd1339011eeeafec1f8f624f72
Chris Thompson <chris@233analytics.com>
2021-11-29 22:48:22 -0600

    fixed a bad regex call

--- src/views/protected/AdminViews/AdminPage.tsx
+++ src/views/protected/AdminViews/AdminPage.tsx
@@ -82,7 +82,7 @@ const handleChange = (
     setSelectedTables(value as string[])
 }
 
-const ISO_DATE_REG = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/.compile()
+const ISO_DATE_REG = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/
 
 /**
  *
