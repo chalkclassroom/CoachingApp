97481a49e09720b10ccbd6400582df01ff2839c0
Zach Johnson <Zachjdev2121@gmail.com>
2022-01-17 14:45:54 -0500

    Add environment params for staging and dev. Add an NPM script to run functions with environment

--- .env-cmdrc.js
+++ .env-cmdrc.js
@@ -9,24 +9,34 @@ module.exports = {
     "messagingSenderId": "353838544707",
     "measurementId": "G-S797QZ8L3N"
   }`,
+  FIREBASE_PROJECT_ID: 'cqrefpwa'
 },
   "development": {
     /*
-    TODO: Replace with different environment? this is currently the production config with the emulator suite
+    Same Environment as staging; emulators will capture firestore / functions
      */
     "REACT_APP_FIREBASE_CONFIG": `{
-    "apiKey": "AIzaSyB7IUNOBelyA5-rMBSM4PtADvlvUOqe6NU",
-    "authDomain": "cqrefpwa.firebaseapp.com",
-    "databaseURL": "https://cqrefpwa.firebaseio.com",
-    "projectId": "cqrefpwa",
-    "storageBucket": "cqrefpwa.appspot.com",
-    "messagingSenderId": "353838544707",
-    "measurementId": "G-S797QZ8L3N"
-  }`,
+  apiKey: "AIzaSyAdl6szTzbtdD3iq8VoS86ZsMWSxUFtaJ4",
+  authDomain: "chalk-dev-c6a5d.firebaseapp.com",
+  projectId: "chalk-dev-c6a5d",
+  storageBucket: "chalk-dev-c6a5d.appspot.com",
+  messagingSenderId: "602770325418",
+  appId: "1:602770325418:web:9a2724b5619afe295cf5d3"
+}`,
     REACT_APP_USE_LOCAL_FIREBASE : true,
     REACT_APP_USE_LOCAL_FUNCTIONS: true,
+    FIREBASE_PROJECT_ID: 'chalk-dev-c6a5d-local'
   },
   "staging": {
-    // TODO: Get new QA Firebase environment for testing
+
+    "REACT_APP_FIREBASE_CONFIG": `{
+  apiKey: "AIzaSyAdl6szTzbtdD3iq8VoS86ZsMWSxUFtaJ4",
+  authDomain: "chalk-dev-c6a5d.firebaseapp.com",
+  projectId: "chalk-dev-c6a5d",
+  storageBucket: "chalk-dev-c6a5d.appspot.com",
+  messagingSenderId: "602770325418",
+  appId: "1:602770325418:web:9a2724b5619afe295cf5d3"
+}`,
+    FIREBASE_PROJECT_ID: 'chalk-dev-c6a5d'
   }
 }
