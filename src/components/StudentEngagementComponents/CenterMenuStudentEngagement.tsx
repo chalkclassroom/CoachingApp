27dd0cfbd71fdaca1cfae308671352e91ff6d691
Zach Johnson <Zachjdev2121@gmail.com>
2022-02-25 13:25:30 -0500

    fix incorrect handling of off-task engagement

--- src/components/StudentEngagementComponents/CenterMenuStudentEngagement.tsx
+++ src/components/StudentEngagementComponents/CenterMenuStudentEngagement.tsx
@@ -199,8 +199,8 @@ class CenterMenuStudentEngagement extends React.Component<Props, State> {
       currentStudent,
       entryType: entryTypeIndex,
     } = this.state
-
-    if (rating && rating !== -1 && currentStudent) {
+// Not what sure rating !== -1 is for, but rating >= 0 will catch any null or undefined values
+    if (rating >= 0 && rating !== -1 && currentStudent) {
       const entryType = entryTypes[entryTypeIndex] || 'none'
 
       const mEntry = {
