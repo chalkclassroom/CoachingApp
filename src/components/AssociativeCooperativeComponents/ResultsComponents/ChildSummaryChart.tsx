6a090d643f118f66f31ade300d70106cd1abc2b1
Zach Johnson <Zachjdev2121@gmail.com>
2022-03-14 15:20:23 -0400

    Refactor Associative / Cooperative Interactions

---
+++ src/components/AssociativeCooperativeComponents/ResultsComponents/ChildSummaryChart.tsx
@@ -0,0 +1,39 @@
+import React, { FunctionComponent } from 'react';
+import Grid from "@material-ui/core/Grid/Grid";
+import * as Constants from "../../../constants/Constants";
+import ChildBehaviorsPie from "./ChildBehaviorsPie";
+import GraphHeader from "../../LayoutComponents/GraphLayouts/GraphHeader";
+import PieChartLegend from "../../LayoutComponents/GraphLayouts/PieChartLegend";
+
+interface OwnProps {
+  ac: number
+  noAc: number
+  noChildOpp: number
+}
+
+type Props = OwnProps;
+
+const ChildSummaryChart: FunctionComponent<Props> = (props) => {
+
+  return (
+    <div>
+      <Grid container justify={"center"} direction={"column"}>
+        <GraphHeader graphTitle={'Child Behaviors'}/>
+        <PieChartLegend slices={[
+          {color:Constants.Colors.AC,
+          label:"Engaged in associative and cooperative interactions."},
+          {color: Constants.Colors.RedGraph,
+          label: "Played in the same area but did not interact."},
+          {color: '#bababa', label: "Played alone (had no opportunity for interaction)."}
+        ]} legendTitle={'Compare how often the children:'}/>
+        <ChildBehaviorsPie
+          ac={props.ac}
+          noAc={props.noAc}
+          noChildOpp={props.noChildOpp}
+        />
+      </Grid>
+    </div>
+  );
+};
+
+export default ChildSummaryChart;
