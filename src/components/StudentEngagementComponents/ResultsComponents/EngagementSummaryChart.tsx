40da0c1b58448ef7232f45b931972252afa018b6
Zach Johnson <Zachjdev2121@gmail.com>
2022-03-14 15:31:58 -0400

    Refactor Student Engagement chart

--- src/components/StudentEngagementComponents/ResultsComponents/EngagementSummaryChart.tsx
+++ src/components/StudentEngagementComponents/ResultsComponents/EngagementSummaryChart.tsx
@@ -1,21 +1,26 @@
-import Grid from "@material-ui/core/Grid/Grid";
-import PieSummary from "./PieSummary";
-import * as React from "react";
-import EngagementSummaryChartHeader from "./EngagementSummaryChartHeader";
-import EngagementSummaryChartLegend from "./EngagementSummaryChartLegend";
+import Grid from '@material-ui/core/Grid/Grid'
+import PieSummary from './PieSummary'
+import * as React from 'react'
 
+import * as Constants from '../../../constants/Constants'
+import PieChartLegend from '../../LayoutComponents/GraphLayouts/PieChartLegend'
 
-export default function EngagementSummaryChart (props: { offTask: number; engaged: number; }) {
-
+export default function EngagementSummaryChart(props: {
+  offTask: number
+  engaged: number
+}) {
   return (
     <div>
-      <Grid container justify={"center"} direction={"column"}>
-        <EngagementSummaryChartHeader/>
-        <EngagementSummaryChartLegend/>
-        <PieSummary
-          offTask={props.offTask}
-          engaged={props.engaged}
+      <Grid container justify={'center'} direction={'column'}>
+        <PieChartLegend
+          slices={[
+            { color: Constants.Colors.SE, label: 'Engaged' },
+            { color: Constants.Colors.RedGraph, label: 'OffTask' },
+          ]}
+          legendTitle={'Compare how often the students were:'}
         />
+
+        <PieSummary offTask={props.offTask} engaged={props.engaged} />
       </Grid>
     </div>
   )
