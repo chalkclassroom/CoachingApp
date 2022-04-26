53137e289011198d3545f02469ad3203df52353d
ZachJDev <57590037+ZachJDev@users.noreply.github.com>
2022-04-26 10:24:37 -0400

    #444 update to push entire observation on creation

--- functions/observationToBQ/index.js
+++ functions/observationToBQ/index.js
@@ -10,18 +10,17 @@ const firestore = new Firestore({
 
 exports.observationsToBQ = functions.firestore
     .document("/observations/{observationID}")
-    .onUpdate((change, context) => {
+    .onCreate((snapshot, context) => {
         // Get an object representing the document
         // e.g. {'name': 'Marie', 'age': 66}
-        const newValue = change.after.data();
+        const newValue = snapshot.data();
 
         // ...or the previous value before this update
-        const previousValue = change.before.data();
+        // const previousValue = change.before.data();
 
         // access a particular field as you would any JS property
-        if (newValue.end !== previousValue.end) {
             console.log("Session Finished");
-            console.log(`Newv value is ${JSON.stringify(newValue)}`);
+            console.log(`New value is ${JSON.stringify(newValue)}`);
 
             // perform desired operations ...
             let datasetName = functions.config().env.bq_dataset;
@@ -59,9 +58,9 @@ exports.observationsToBQ = functions.firestore
                     .then(entries => {
                         entries.forEach(entry => {
                             console.log(entry.id, "=>", entry.data());
-
                             let entryData = entry.data();
                             if( entryData.Type === "UNDO"){
+                              //TODO: This could inadvertently pop a tone rating, which does not match the behavior in app.
                                 rows.pop();
                             } else if( entryData.Type === "Rat"){
                                 let row = {
@@ -707,5 +706,4 @@ exports.observationsToBQ = functions.firestore
               } else {
                 console.log("Next Magic 8 will be filled");
             }
-        }
     });
