ed92704afcef89e2ca48b0979f1dc971eb5ba2cf
areynol <ajr2934@outlook.com>
2022-08-19 16:43:39 -0400

    fixed scrolling issue for math

--- src/components/MathInstructionComponents/ResultsComponents/TeacherBarDetails.tsx
+++ src/components/MathInstructionComponents/ResultsComponents/TeacherBarDetails.tsx
@@ -122,10 +122,9 @@ class TeacherBarDetails extends React.Component<Props, {}> {
                 }
               }
             }
-          }
+          },
+          maintainAspectRatio: false
         }}
-        width={650}
-        height={400}
       />
     );
   }
