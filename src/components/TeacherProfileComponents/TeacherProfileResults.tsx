73db4bda9575995b7aa42dca4cb78c3943f9370c
Chase Childers <chasechilders12@gmail.com>
2022-11-15 11:20:42 -0500

    added colors for engagement average

--- src/components/TeacherProfileComponents/TeacherProfileResults.tsx
+++ src/components/TeacherProfileComponents/TeacherProfileResults.tsx
@@ -261,6 +261,8 @@ const chartTitleArr = {
   transitionGroupAverage: 'Transition',
   centersGroupAverage: 'Centers',
 
+  dailyAverage: "Daily Average",
+
   eyeLevelAverage: 'At Eye Level',
   positiveExpressionAverage:
     'Uses positive or interested expression to encourage child talk',
@@ -369,6 +371,9 @@ const lineColorChoices = {
     "Teacher Asks Low-Level Question": "#1155CC",
     "Child Answers Low-Level Question": "#1155CC",
   },
+  "studentEngagement" : {
+    "Daily Average": "#FF7F00"
+  }
 }
 
 const AVERAGES_SUBPAGE = 0;
