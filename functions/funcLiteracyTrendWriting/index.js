176fb5677d3caea8d45a48a3a547f37759cee9f5
Matt Haslem <matt@matthaslem.tech>
2025-08-11 11:43:04 -0400

    Upgrade Node version for Cloud Functions using updated accessUtils function

--- functions/funcLiteracyTrendWriting/index.js
+++ functions/funcLiteracyTrendWriting/index.js
@@ -14,6 +14,7 @@ const bigquery = new BigQuery();
  */
 exports.funcLiteracyTrendWriting = functions.https.onCall(async(data, context) => {
     if (!await canAccessTeacher(data.teacherId, context.auth.uid)){
+        console.log(`User ${context.auth.uid} can NOT access observation ${data.sessionId}`)
         return [];
     }else{
         console.log(`User ${context.auth.uid} can access observation ${data.sessionId}`)
--- functions/funcTransitionSessionDates/index.js
+++ functions/funcTransitionSessionDates/index.js
@@ -17,6 +17,7 @@ exports.funcTransitionSessionDates = functions.https.onCall( async (data, contex
   console.log(context.auth.uid);
   console.log(data.teacherId);
   if (!await canAccessTeacher(data.teacherId, context.auth.uid)){
+    console.log(`User ${context.auth.uid} can NOT access teacher ${data.teacherId}`)
     return [];
   }else{
     console.log(`User ${context.auth.uid} can access teacher ${data.teacherId}`)
