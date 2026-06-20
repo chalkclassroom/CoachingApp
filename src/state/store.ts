c71287fc46480a6f1fcc6cdca22f7cbccf1de79a
Ismael Dosil <ismaeldosil@gmail.com>
2026-06-19 23:08:50 -0300

    fix(observe): stop persisting cached teacher roster so it refetches
    Closes CHALK-2

--- src/state/store.ts
+++ src/state/store.ts
@@ -7,6 +7,7 @@ import rootReducer from './root-reducer'
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['teacherListState']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
